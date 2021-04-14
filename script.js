0a758005ab1af688f905d2e902439f731f2ecb26
Rafael Ribeiro <rafittu@outlook.com>
2021-04-14 20:46:07 -0300

    requisitos 2 e 3

--- script.js
+++ script.js
@@ -47,14 +47,35 @@ fetchComputers();
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function cartItemClickListener(event) {
+  document.querySelector('.cart__items').removeChild(event.target);
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const addToCart = (id) => {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const product = {
+      sku: data.id,
+      name: data.title,
+      salePrice: data.price,
+    };
+    document.querySelector('.cart__items').appendChild(createCartItemElement(product));
+  });
+};
+
+document.body.addEventListener('click', (event) => {
+  const classList = event.target.className;
+  if (classList.includes('item__add')) {
+    const item = event.target.parentElement.firstChild;
+    addToCart(item.innerText);
+  }
+});
