bb0ad8d9d531ed2ffe47d1918adc7e919b58838a
Stanrley Barbosa <3011stan@gmail.com>
2021-04-14 19:25:48 -0300

    Requisito 1 do projeto

--- script.js
+++ script.js
@@ -1,43 +1,28 @@
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
-
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
-
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  const obj = {
-    sku,
-    name,
-    image,
-  };
-  section.className = 'item';
-  return obj;
-
-  // section.appendChild(createCustomElement('span', 'item__sku', sku));
-  // section.appendChild(createCustomElement('span', 'item__title', name));
-  // section.appendChild(createProductImageElement(image));
-  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  // return section;
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
 }
 
-window.onload = function onload() {
-  const obj = {
-    sku: 123, 
-    name: 'nomeTeste', 
-    image: 'https://startupi.com.br/wp-content/uploads/2020/04/desenvolvimento-low-code.jpeg',
-  };
-  createProductItemElement(obj);
-};
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductItemElement({ id, title, thumbnail }) {
+  const section = document.createElement('section');
+  // console.log(id, title, thumbnail);
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+}
+
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
@@ -53,3 +38,28 @@ window.onload = function onload() {
 //   li.addEventListener('click', cartItemClickListener);
 //   return li;
 // }
+async function foundItems(endPoint) {
+  return new Promise((resolve) => {
+    fetch(endPoint)
+      .then((response) => {
+        response.json()
+          .then((data) => {
+            resolve(data.results);
+          });
+      });
+      // reject(new Error('endpoint não existe.'));
+  });
+}
+
+async function setItems() {
+  const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
+  const datas = await foundItems(endPoint);
+  datas.forEach((data) => {
+    const newElement = createProductItemElement(data);
+    document.querySelector('.items').appendChild(newElement);
+  });
+}
+
+window.onload = function onload() {
+  setItems();
+};
