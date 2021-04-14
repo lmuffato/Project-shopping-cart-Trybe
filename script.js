8ebf45b3f4d54fa0c9c02e3f9ae5a73909b1895a
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-04-14 16:22:55 -0400

    Requisito 1 - Crie uma listagem de produtos.

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
-// window.onload = function onload() { };
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
 // function cartItemClickListener(event) {
-//   // coloque seu código aqui
+  // coloque seu código aqui
 // }
 
 // function createCartItemElement({ sku, name, salePrice }) {
@@ -41,3 +39,25 @@
 //   li.addEventListener('click', cartItemClickListener);
 //   return li;
 // }
+
+// Requisito 1 - Crie uma listagem de produtos.
+
+async function recoverMercadoLivreResults(term) {
+  const endpoint = `https://api.mercadolibre.com/sites/MLB/search?q=${term}`;
+
+  const response = await fetch(endpoint);
+  const object = await response.json();
+  // const obj = object.results;
+  const { results: obj } = object;
+  const itemsElement = document.querySelector('.items');
+
+  obj.forEach((result) => {
+    const { id: sku, title: name, thumbnail: image } = result;
+    const element = createProductItemElement({ sku, name, image });
+    itemsElement.appendChild(element);
+  });
+}
+
+window.onload = function onload() { 
+  recoverMercadoLivreResults('computador');
+};
