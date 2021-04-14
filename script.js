b9fdf562e4bb836b7d9b44390c459de2479e9a6e
Edrei <edreimateus@gmail.com>
2021-04-14 18:16:06 -0300

    eslint issues

--- script.js
+++ script.js
@@ -24,6 +24,19 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function cartItemClickListener(event) {
+  event.target.remove();
+  localStorage.removeItem(localStorage.key(event));
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
  function addItens(itemId) {
    return new Promise((resolve) => {
     fetch(`https://api.mercadolibre.com/items/${itemId}`)
@@ -33,15 +46,16 @@ function createProductItemElement({ sku, name, image }) {
         throw new Error(data.error);
       }
       const { id: sku, title: name, price: salePrice } = data;
+      const element = createCartItemElement({ sku, name, salePrice });
       const cartShopping = document.querySelector('.cart__items');
-      cartShopping.appendChild( createCartItemElement({ sku, name, salePrice }));
+      cartShopping.appendChild(element);
       resolve();
     })
     .catch((error) => {
       console.log(error);
-    })
+    });
    });
- };
+ }
 
 function fetchComputers(product) {
   return new Promise((resolve) => {
@@ -67,19 +81,6 @@ function fetchComputers(product) {
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
-
-function cartItemClickListener(event) {
-  event.target.remove();
-  localStorage.removeItem(localStorage.key(event));
-}
-
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
 
 window.onload = function onload() {
   fetchComputers('computador');
