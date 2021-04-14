ce5104c09466e2b76bf07c7027600af4a75cf079
AdelinoJnr <adelinojunior96@gmail.com>
2021-04-14 18:20:22 -0300

    Resoluçao do requisito 1 (Colocando funçao no onload)

--- script.js
+++ script.js
@@ -1,56 +1,92 @@
-window.onload = function onload() { };
-
-const linkUrl = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-const numberRandom = Math.ceil(Math.random() * 50);
+const erro = 'Não encontrei nada';
 
 function createProductImageElement(imageSource) {
-  const sectionItens = document.querySelector('.items');
   const img = document.createElement('img');
   img.className = 'item__image';
-  img.src = imageSource.results[numberRandom].thumbnail;
-  sectionItens.appendChild(img);
+  img.src = imageSource;
   return img;
 }
 
-const dados = () => {
-  fetch(linkUrl).then((result) => result.json().then((data) => {
-    createProductImageElement(data);
-  }));
-};
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-dados();
+function createProductItemElement({ id, title, thumbnail }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
-
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
 // function cartItemClickListener(event) {
-//   // coloque seu código aqui
+//   console.log('cliclei no li');
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
+// function createCartItemElement({ id, title, price }) {
 //   const li = document.createElement('li');
 //   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
 //   li.addEventListener('click', cartItemClickListener);
+//   console.log(li);
 //   return li;
 // }
+
+// const dadosAPI = async () => {
+//   const dados = await fetch('https://api.mercadolibre.com/items/MLB1341706310');
+//   const dadosValue = await dados.json();
+//   return dadosValue;
+// };
+
+// const createValue = async () => {
+//   try {
+//     createCartItemElement(await dadosAPI());
+//   } catch (error) {
+//     return erro;
+//   }
+// };
+
+// console.log(dadosAPI().then((dados) => console.log(dados)));
+// createValue();
+
+/*
+Funçoes 'dadosAPI' , 'appendListComputers' , 'createDados' 
+foram criadas atravez de um Code Review do meu colega Renzo Sevilha
+com a ideia de nao utilizar o 'then', mas sim usar funcoes 'async'
+*/
+
+const dadosAPIPcs = async () => {
+  const requisicao = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const objDados = await requisicao.json();
+  return objDados;
+};
+
+const appendComputers = (dados) => {
+  dados.results.forEach((result) => {
+    const sectionItens = document.querySelector('.items');
+    sectionItens.appendChild(createProductItemElement(result));
+  });
+};
+
+const createDados = async () => {
+  try {
+    appendComputers(await dadosAPIPcs());
+  } catch (error) {
+    return erro;
+  }
+};
+
+window.onload = function onload() {
+  createDados();
+};
