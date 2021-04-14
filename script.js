52567ac4de0178d7f56f36f8367b7d237e112945
defauth98 <neto.daniribeiro@gmail.com>
2021-04-14 15:40:43 -0300

    Requisito 02: Adicionando item ao carrinho

--- script.js
+++ script.js
@@ -24,6 +24,37 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const addToCart = (itemId) => {
+  fetch(`https://api.mercadolibre.com/items/${itemId}`)
+    .then(async (response) => {
+      const jsonReponse = await response.json();
+
+      const item = {
+        sku: jsonReponse.id,
+        name: jsonReponse.title,
+        salePrice: jsonReponse.price,
+      };
+
+      console.log(item);
+
+      const cartItem = createCartItemElement(item);
+      const cartList = document.getElementsByClassName('cart__items')[0];
+      cartList.appendChild(cartItem);
+    });
+};
+
 window.onload = function onload() {
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then(async (response) => {
@@ -37,6 +68,7 @@ window.onload = function onload() {
 
       products.forEach((product) => {
         const newItem = createProductItemElement(product);
+        newItem.onclick = () => addToCart(product.sku);
         const items = document.getElementsByClassName('items')[0];
 
         items.appendChild(newItem);
@@ -46,16 +78,4 @@ window.onload = function onload() {
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
 // }
