16aeb977dc310144b5da4b51230a8a99166c0f37
Felipe Flores <felipefloresvendas@gmail.com>
2021-04-14 15:49:50 -0300

    Requirement 1 Done

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-// window.onload = function onload() { };
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+const fetchResponse = async (URL) => {
+  const response = await fetch(URL);
+  const jsonResponse = await response.json();
+  return jsonResponse;
+};
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+const foundItemsByType = async (query = 'computador') => {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  return fetchResponse(URL);
+};
 
-//   return section;
-// }
+const createItemsSection = (data) => {
+  const items = document.querySelector('.items');
+  data.results.forEach((item) => {
+    const object = { sku: item.id, name: item.title, image: item.thumbnail };
+    const element = createProductItemElement(object);
+    items.appendChild(element);
+  });
+};
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -41,3 +57,11 @@
 //   li.addEventListener('click', cartItemClickListener);
 //   return li;
 // }
+
+window.onload = async function onload() {
+  try {
+    await foundItemsByType().then((data) => createItemsSection(data));
+  } catch (error) {
+    alert(error);
+  }
+};
