71b18086b15d315c1e50bfbdc040b5167af95a24
Giovanni <giovannitadeu08@gmail.com>
2021-04-14 18:04:16 -0300

    teste

--- script.js
+++ script.js
@@ -24,29 +24,52 @@ function createProductItemElement({ sku, name, image }) {
 
 /* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
-
-function cartItemClickListener(event) {
-  // coloque seu código aqui
 } */
 
-/* function createCartItemElement({ sku, name, salePrice }) {
+function cartItemClickListener(event) {
+  const containerItems = document.querySelector('.cart__items');
+  containerItems.removeChild(event.target);
+} 
+
+function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
 
 async function fetchAndRenderProduct() {
-const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-const { results } = await response.json();
-results.forEach((product) => {
-  const producItem = createProductItemElement(product);
-  document.querySelector('.items').appendChild(producItem);
-});
+  const selectItems = document.querySelector('.items');
+  await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((response) => response.json())
+    .then((data) => {
+      data.results.forEach((result) => {
+        const { id: sku, title: name, thumbnail: image } = result;
+        selectItems.appendChild(createProductItemElement({ sku, name, image }));
+      });
+    });
+}
+
+function saveItemsLocalStorage() {
+  const cartItems = document.querySelector('.cart__items').innerHTML;
+  localStorage.setItem('cart__items', cartItems);
+}
+
+async function addItemCart(event) {
+  const id = event.target.parentNode.firstChild.innerText;
+  const containerCart = document.querySelector('.cart_items');
+  const itemFetch = await fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => response.json())
+    .then((data) => data);
+  const { id: sku, title: name, price: salePrice } = itemFetch;
+  const carItem = createCartItemElement({ sku, name, salePrice });
+  containerCart.appendChild(carItem);
 }
 
 window.onload = function onload() { // chama as funções ao carregar a pagina
   fetchAndRenderProduct();
+  addItemCart();
+  saveItemsLocalStorage();
+  cartItemClickListener();
 };
