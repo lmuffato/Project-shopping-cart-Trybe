2659024298b3a30954077d5150f4835530126796
rogerio <rogerio.cl@gmail.com>
2021-04-14 15:38:55 -0300

    commit inicial e primeiro requisito

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -26,18 +25,32 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
+
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
+
+async function loadProducts() {
+  const recoveryData = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((result) => result.json())
+    .then((result) => result.results)
+  recoveryData.forEach((computer) => {
+    const computerObject = {sku: computer.id, name: computer.title, image: computer.thumbnail};
+    console.log(computerObject);
+    document.querySelector('.items').appendChild(createProductItemElement(computerObject));
+  });
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+loadProducts();
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
