7c4d168263a35656805aee4ac11db30ee0b46705
Elisa <elisafranca.carvalho@gmail.com>
2021-04-14 19:26:09 -0300

    Requisito 1: Cria uma listagem de produtos

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
-window.onload = function onload() { };
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-//   return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -41,3 +39,25 @@ window.onload = function onload() { };
 //   li.addEventListener('click', cartItemClickListener);
 //   return li;
 // }
+
+// Para essa função contei com o auxílio das dicas do Patrick Morais no slack.
+const getObjectItems = async () => {
+  const endPoint = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const objectJson = await endPoint.json();
+  const items = objectJson.results;
+
+  items.forEach((item) => {
+    const newObject = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    };
+    const itemsElement = document.querySelector('.items');
+    const creatingElement = createProductItemElement(newObject);
+    itemsElement.appendChild(creatingElement);
+  });
+};
+
+window.onload = function onload() { 
+  getObjectItems();
+};
