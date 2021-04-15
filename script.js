c1676212cde91ca253c09aa806063383f57107e5
Bruno Mendes <mendessbruno@gmail.com>
2021-04-15 17:00:10 -0300

    refatoraçao do modo como armazena dados no local storage para resoluçao do ex4

--- script.js
+++ script.js
@@ -6,31 +6,38 @@ const clearList = () => {
 
 // inicio funçoes do local storage
 
-const addLocalStorage = (string) => {
-  let storage;
+const addLocalStorage = (object) => {
+  let storage = [];
   if (localStorage.getItem('cart') !== null) {
-    storage = `${localStorage.getItem('cart')};${string}`;
-  } else storage = string;
+    storage = JSON.parse(localStorage.getItem('cart'));
+    storage.push(object);
+  } else storage.push(object);
 
-  localStorage.setItem('cart', storage);
-};
-
-const removeLocalStorage = (sku) => {
-  const storage = localStorage.getItem('cart');
-  const array = storage.split(';');
-  let removed;
-  array.forEach((element) => {
-    if (element !== sku) {
-      if (removed === undefined) {
-        removed = `${element}`;
-      } else removed = `${removed};${element}`;
-    }
-  });
-  localStorage.setItem('cart', removed);
+  localStorage.setItem('cart', JSON.stringify(storage));
 };
 
 const clearStorage = () => {
   localStorage.removeItem('cart');
+};
+  
+const sideFunction = (storage, sku) => {
+  const removed = [];
+    storage.forEach((object) => {
+    if (object.sku !== sku) {
+      removed.push(object);
+    }
+  });
+  localStorage.setItem('cart', JSON.stringify(removed));
+};
+
+const removeLocalStorage = (sku) => {
+  const storage = JSON.parse(localStorage.getItem('cart'));
+  if (storage.length === 1) {
+    clearStorage();
+  } else {
+    sideFunction(storage, sku);
+    console.log(sku);
+}
 };
 
 // fim funções do local storage
@@ -60,14 +67,14 @@ function createProductItemElement({ sku, name, image }) {
 
   return section;
 }
-
+  
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
 // Exercicio 3 inicio
 
-function cartItemClickListener(event) {
+async function cartItemClickListener(event) {
   const id = event.target.innerText.split(' ')[1];
   removeLocalStorage(id);
   event.target.remove();
@@ -85,7 +92,7 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 // Exercicio 6 inicio
 
-const clearCart = () => {
+const ListenClearBtn = () => {
   const btn = document.querySelector('.empty-cart');
   btn.addEventListener('click', () => {
     clearStorage();
@@ -97,66 +104,65 @@ const clearCart = () => {
 
 // begin update cart (exercicio4)
 
+const appendCartItem = (object) => {
+  const cart = document.querySelector('.cart__items');
+  cart.appendChild(createCartItemElement(object));
+};
+
+const updateCart = () => {
+  const storage = JSON.parse(localStorage.getItem('cart'));
+  if (storage !== null) {
+    storage.forEach((object) => appendCartItem(object));
+  }
+};
+
+// end update cart (exercicio4)
+
+// Inicio ex 2 (add to cart)
+
+// const fetch = require('node-fetch'); 
+
 const createCartObject = ({ id, title, price }) => ({
   sku: id,
   name: title,
   salePrice: price,
 });
 
-const appendCartItem = (object) => {
-  const cart = document.querySelector('.cart__items');
-  cart.appendChild(createCartItemElement(createCartObject(object)));
-};
-
-const fetchId = (id) => {
+async function fetchId(id) {  
   const APIUrlId = `https://api.mercadolibre.com/items/${id}`;
-  
+
   const myObject = {
     method: 'GET',
     headers: { Accept: 'application/json' },
   };
-  fetch(APIUrlId, myObject)
+  return fetch(APIUrlId, myObject)
   .then((response) => response.json())
-  .then((data) => appendCartItem(data));
-};
-
-const updateCart = () => {
-  const storage = localStorage.getItem('cart');
-  if (storage !== null) {
-    const array = storage.split(';');
-    array.forEach((element) => fetchId(element));
-  }
-};
-
-// end update cart (exercicio4)
-
-// Inicio ex 2
-
-const addToCart = () => {
+  .then((data) => addLocalStorage(createCartObject(data)));
+}
+  
+async function addToCart() {
   const buttons = document.querySelectorAll('.item__add');
   buttons.forEach((item) => {
-    item.addEventListener('click', () => {
+    item.addEventListener('click', async () => {
       const id = getSkuFromProductItem(item.parentElement);
-      addLocalStorage(id);
+      await fetchId(id);
       clearList();
       updateCart();
     });
   });
-};
+}
 
 // Final resolução ex 2
 
 // Início resolução exercicio 1
 
-const APIUrlComputador = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
-
 const tratarElemento = (objeto) => ({
-    sku: objeto.id,
-    name: objeto.title,
-    image: objeto.thumbnail,
-    salePrice: objeto.price,
-  });
-
+  sku: objeto.id,
+  name: objeto.title,
+  image: objeto.thumbnail,
+  salePrice: objeto.price,
+});
+  
 const createItem = (array) => {
   const items = document.querySelector('.items');
   array.forEach((element) => {
@@ -166,6 +172,8 @@ const createItem = (array) => {
 };
 
 const fetchAPI = () => {
+  const APIUrlComputador = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+
   const myObject = {
     method: 'GET',
     headers: { Accept: 'application/json' },
@@ -202,7 +210,7 @@ const start = () => {
 // Final resolução exercicio 7
 
 window.onload = function onload() { 
-  start();
-  clearCart();
-  updateCart();
-};
+    updateCart();
+    start();
+    ListenClearBtn();
+  };
