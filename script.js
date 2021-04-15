e2f54e5571f25eaa06bb38426eec056c94874155
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-04-15 16:17:35 -0300

    Requisito 2 - Ajustado para passar no Evaluator

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function cartItemClickListener(event) {
   event.target.remove();
 }
 
-function createCartItemElement({ id: sku, title: name, base_price: salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
