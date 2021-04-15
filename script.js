83b5df8599f453e0d9cbe0c16e5e110e291c5a88
Beatriz Barbosa <beatrizcpbarbosa@gmail.com>
2021-04-15 16:54:15 -0300

    requisito 2 e 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// projeto ja pronto
+// Crie uma listagem de produtos
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -29,42 +29,72 @@ function createProductItemElement({ id, title, thumbnail }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
-
 // pegando o produto com api do mercado livre
-
 function getProduct() {
-  let produtos;
+  return new Promise((resolve) => {
+    let produtos;
 
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       produtos = data.results;
       produtos.forEach((produto) => createProductItemElement(produto)); 
-    });   
+      resolve();
+    }); 
+  });  
+}
+// fim de Crie uma listagem de produtos
+
+// Crie uma listagem de produtos no cart
+// Remova o item do carrinho de compras ao clicar nele
+function cartItemClickListener(event) {
+  const clicarItem = event.target;
+  clicarItem.remove();
 }
 
-// function getItem(id) {
-//   fetch(`https://api.mercadolibre.com/items/${id}`)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// }
+// crio os elementos html / adiciono o produto ao carrinho de compras
+function createCartItemElement({ id, title, price }) {
+  const li = document.createElement('li');
+  const ol = document.querySelector('.cart__items');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  li.addEventListener('click', cartItemClickListener);
+  const addFilho = ol.appendChild(li);
+  return addFilho;
+}
 
+// pego o conteudo do produto especifico pela api com o id do produto especifico
+function getItem(id) {
+  let itemAdd;
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      itemAdd = data;
+      createCartItemElement(itemAdd);
+    });
+}
+
+// uso essa função para buscar o conteudo do id do produto especifico
+function getIDFromProductItem(item) {
+  const iditem = item.querySelector('span.item__sku').innerText;
+  return getItem(iditem);
+}
+
+// função para cliclar no botão e pegar o conteudo especifico desse clique
+function clickAddToCart() {
+  const btnAddToCart = document.querySelectorAll('.item__add');
+  btnAddToCart.forEach((index) => {
+      index.addEventListener('click', (event) => {
+      const clicarBtn = event.target.parentNode;
+      getIDFromProductItem(clicarBtn);
+    });
+  });
+}
+// fim de crie uma listagem de produtos no cart
+
+// onload
 window.onload = function onload() {
-  getProduct();
+  getProduct().then(() => clickAddToCart());
 };
