2d3922d21c31ca049e048e54a13685c295786dea
Marcus C. H. Filho <mchusemann@gmail.com>
2021-04-15 10:35:39 -0300

    :rocket: feat(Requisito 1): Criar listagem de produtos

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-// window.onload = function onload() { };
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(
+    createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'),
+  );
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  return section;
+}
+const appendElement = (element) => {
+  const elementToAppend = createProductItemElement(element);
+  document.querySelector('.items').appendChild(elementToAppend);
+};
 
-//   return section;
-// }
+const createElements = (data) => {
+  console.log(data);
+  data.forEach((item) => {
+    const obj = {
+      sku: item.id,
+      name: item.title,
+      image: item.thumbnail,
+    };
+
+    appendElement(obj);
+  });
+};
+
+const fetchData = async () =>
+  new Promise((resolve, reject) => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+      .then((resp) => resp.json())
+      .then((data) => data.results)
+      .then((elements) => {
+        if (elements) resolve(elements);
+        return reject(new Error('Falha ao buscar os dados!!'));
+      });
+  });
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -41,3 +69,15 @@
 //   li.addEventListener('click', cartItemClickListener);
 //   return li;
 // }
+
+window.onload = async function onload() {
+  try {
+    const data = await fetchData();
+    createElements(data);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // const result = await fetchData();
+  // createProductItemElement(result.results[1]);
+};
