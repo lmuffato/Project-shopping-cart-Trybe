3be705c6cd5e08f27f5c0f780192aded295bb969
nascjoao <oi@joaonasc.dev>
2021-04-14 22:19:54 -0300

    ❗ (Possui erros) Requisito 4: Carrega carrinho do localStorage

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ const products = {
     const data = await response.json();
     return data.results;
   },
+  async getProduct(productId) {
+    const response = await fetch(`https://api.mercadolibre.com/items/${productId}`);
+    const data = await response.json();
+    return data;
+  },
+  addToCart(cart, cartItem) {
+    cart.appendChild(cartItem);
+    let localCart = localStorage.getItem('cart');
+    if (localCart) {
+      localCart = localCart.split(',');
+      localCart.push(cartItem.id);
+      localStorage.setItem('cart', localCart);
+    } else {
+      localStorage.setItem('cart', cartItem.id);
+    }
+  },
   create: {
     createProductImageElement(imageSource) {
       const img = document.createElement('img');
@@ -20,10 +36,14 @@ const products = {
     cartItemClickListener(event) {
       const cartItem = event.target;
       cartItem.remove();
+      let localCart = localStorage.getItem('cart').split(',');
+      localCart = localCart.filter((item) => item !== cartItem.id);
+      localStorage.setItem('cart', localCart);
     },
     createCartItemElement({ id: sku, title: name, price: salePrice }) {
       const li = document.createElement('li');
       li.className = 'cart__item';
+      li.id = sku;
       li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
       li.addEventListener('click', this.cartItemClickListener);
       return li;
@@ -34,7 +54,7 @@ const products = {
       const button = this.createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
       const cartItems = document.querySelector('.cart__items');
       const cartItemElement = this.createCartItemElement(product);
-      button.onclick = () => cartItems.appendChild(cartItemElement);
+      button.onclick = () => products.addToCart(cartItems, cartItemElement);
       section.appendChild(
         this.createCustomElement('span', 'item__sku', product.id),
       );
@@ -55,6 +75,19 @@ const products = {
       ));
     },
   },
+  async loadCart() {
+    const cartItems = document.querySelector('.cart__items');
+    let localCart = localStorage.getItem('cart');
+    if (localCart) {
+      localCart = localCart.split(',');
+
+      localCart.forEach(async (productId) => {
+        const productFound = await this.getProduct(productId);
+        const cartItem = products.create.createCartItemElement(productFound);
+        cartItems.appendChild(cartItem);
+      });
+    }
+  },
 };
 
 // function getSkuFromProductItem(item) {
@@ -64,4 +97,5 @@ const products = {
 window.onload = async function onload() { 
   const productsFound = await products.getProducts();
   products.create.generateListOfProducts(productsFound);
+  products.loadCart();
 };
