c16082fd7c7d3680aa3fe43c171d0b5e7adceaa5
Villefor <franklincrius@gmail.com>
2021-04-15 19:13:43 -0300

    comitao bomba, noite toda fazendo!

--- script.js
+++ script.js
@@ -1,43 +1,76 @@
-// window.onload = function onload() { };
+const cartItems = '.cart__items';
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function getItemsPrices() {
+  const cartLi = document.querySelector(cartItems);
+  localStorage.setItem('items', cartLi.innerHTML);
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   return section;
-// }
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+  return section;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function cartItemClickListener(event) {
+  event.target.remove();
+  getItemsPrices();
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function fetchProducts() {
+  const getProduct = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((result) => result.json())
+    .then((data) => data.results);
+    getProduct.forEach((item) => {
+    const receivedItems = { sku: item.id, name: item.title, image: item.thumbnail };
+    document.querySelector('.items').appendChild(createProductItemElement(receivedItems));
+  });
+}
+
+const createCartItem = () => {
+  const allItems = document.querySelector('.items');
+  allItems.addEventListener('click', async (event) => {
+   const idSku = getSkuFromProductItem(event.target.parentNode);
+   const getItems = await fetch(`https://api.mercadolibre.com/items/${idSku}`)
+    .then((result) => result.json());
+   const getPrice = { sku: idSku, name: getItems.title, salePrice: getItems.price };
+   const cartLoad = createCartItemElement(getPrice);
+   document.querySelector('ol.cart__items').appendChild(cartLoad);
+   getItemsPrices();
+ });
+};
+
+window.onload = function onload() { };
+fetchProducts();
+createCartItem();
