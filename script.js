9414c9096709146e062354524c19ca6cfe709aa8
AndreBarroso <andre_barroso17@hotmail.com>
2021-04-15 17:47:28 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -12,6 +12,41 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Requisito 2
+
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  console.log(event.target); // linha inserida apenas para evitar erro atual do lint
+}
+
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+async function clickButtons(callback) {
+  const arrayButtons = document.querySelectorAll('.item__add');
+  arrayButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      const computer = callback[index]; 
+      fetch(`https://api.mercadolibre.com/items/${computer.id}`)
+        .then((resp) => resp.json())
+          .then((r) => {
+            const objItem = {
+              sku: r.id,
+              name: r.title,
+              salePrice: r.price,
+            }; 
+            const cartItems = document.querySelector('.cart__items');
+            cartItems.appendChild(createCartItemElement(objItem));
+          });
+    });
+  });
+}
+
 //  Requisito 1
 
 function createProductItemElement({ sku, name, image }) {
@@ -45,6 +80,7 @@ async function getProducts() {
       const item = createProductItemElement(objItem);
       items.appendChild(item);
     });
+    clickButtons(produtos);
   }
 
 /*
@@ -54,22 +90,7 @@ function getSkuFromProductItem(item) {
 }
 */
 
-/*
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
-*/
-
-/*
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
-*/
-
 window.onload = function onload() { 
-  getProducts();
+   getProducts();
+   clickButtons();
 };
