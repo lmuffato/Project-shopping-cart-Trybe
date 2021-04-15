f108328df2e4bf63b230026ec03edb727643a236
Eduardo Seije <eduardoseije@gmail.com>
2021-04-15 18:34:27 -0300

    Requisito 1 - teste 2

--- script.js
+++ script.js
@@ -1,61 +1,65 @@
 // const { default: fetch } = require('node-fetch');
 
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
-    const img = document.createElement('img');
-    img.className = 'item__image';
-    img.src = imageSource;
-    return img;
-  }
-  
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
+
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
-    
-function createProductItemElement({ sku, name, image }) {
+
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
-    
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-    
+  
   return section;
 }
-    
+
 function pcList() {
+  let pcData;
   fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   .then((response) => {
     response.json()
     .then((data) => {
-      data.results.forEach((obj) => { 
-        const datas = {
-          sku: obj.id,
-          name: obj.title,
-          image: obj.thumbnail,
-        };
-        return createProductItemElement(datas);
+      pcData = data.results;
+      pcData.forEach((pc) => {
+        createProductItemElement(pc);
       });
     });
   });
 }
-pcList();
+
+async function pcPromise() {
+  try {
+    pcList();
+  } catch (error) {
+    console.log(error);
+  }
+} 
 
 // function getSkuFromProductItem(item) {
-//         return item.querySelector('span.item__sku').innerText;
-// }
-      
-// function cartItemClickListener(event) {}
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+  //         return item.querySelector('span.item__sku').innerText;
+  // }
+  
+  // function cartItemClickListener(event) {}
+  
+  // function createCartItemElement({ sku, name, salePrice }) {
+    //   const li = document.createElement('li');
+    //   li.className = 'cart__item';
+    //   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+    //   li.addEventListener('click', cartItemClickListener);
+    //   return li;
+    // }
+    
+    window.onload = function onload() { pcPromise(); };
