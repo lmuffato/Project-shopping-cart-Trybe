18aea827139d9999a1648c43c82b4b33977c3e0d
PollyanaOliveira <pollyana.deoliveir@gmail.com>
2021-04-16 17:31:47 -0400

    Requisito 4 - Carregue o carrinho de compras através do LocalStorage ao iniciar a página.

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//           Requisito 3 - Função Nativa
-function cartItemClickListener(event) {
-  event.target.remove();
-}
-
 //                        Requisito 1 - Crie uma listagem de produtos.
 
 //              Função Nativa - Requisito 1              //
@@ -51,10 +46,42 @@ async function recoverMercadoLivreResults(term) {
   });
 }
 
+//                        Requisito 4 - Carregue o carrinho de compras através do LocalStorage ao iniciar a página.
+// const productPrice = document.querySelector('.total-price');
+
+// salvar carrinho no localStorage
+function storeCart() {
+  const shopCart = document.getElementsByClassName('cart__items')[0];
+  localStorage.setItem('cart', shopCart.innerHTML);
+  // localStorage.setItem('productValue', productPrice.innerHTML);
+}
+
+//                        Requisito 3 - Remova o item do carrinho de compras ao clicar nele.
+//              Função Nativa
+function cartItemClickListener(event) {
+  event.target.remove();
+  storeCart(); // para remover do localStorage - Requisito 4
+}
+
+// Função auxiliar para o Requisito 4
+// função para reinserir lista de produtos do localStorage
+// sempre será executado ao abrir a página, ou seja, criará o evento
+
+function updatePage() {
+    const cart = document.querySelector('.cart__items');
+    cart.innerHTML = localStorage.getItem('cart');
+    const recoverItems = document.querySelectorAll('.cart__item');
+    // adiciona o evento click no cart__item que está no carrinho de compras 
+   [...recoverItems].forEach((listItem) => 
+   listItem.addEventListener('click', cartItemClickListener));
+    // productPrice.innerHTML = localStorage.getItem('productValue');
+}
+
 //                        Requisito 2 - Adicione o produto ao carrinho de compras
 
 //              Função Nativa - Requisito 2 - captura id dos produtos             //
 function getSkuFromProductItem(item) {
+  console.log(item);
   return item.querySelector('span.item__sku').innerText;
 }
 
@@ -69,25 +96,27 @@ function createCartItemElement({ sku, name, salePrice }) {
 
 function addProductToCart() {
   // quando o usuário clicar no botão "Adicionar ao Carrinho!" que está em section class='items' (seção dos produtos)
-  // e vai adicionar o produto na ol class='cart__items'
+  // e vai adicionar o produto na ol class='cart__items'  
   const selectItems = document.querySelector('.items');
   selectItems.addEventListener('click', async (event) => {
-    // para retornar o elemento pai:
     const id = getSkuFromProductItem(event.target.parentNode);
     const response = await fetch(`https://api.mercadolibre.com/items/${id}`);
     const object = await response.json();
-   const item = {
+    // const item = { sku: id, name: object.title, salePrice: object.price };
+    const item = {
       sku: id,
       name: object.title,
       salePrice: object.price,
-    };
-    const getItemsCart = document.querySelector('.cart__items');
+    }; 
     const getItemCart = createCartItemElement(item);
-    getItemsCart.appendChild(getItemCart); 
+    const getItemsCart = document.querySelector('.cart__items');
+    getItemsCart.appendChild(getItemCart); // novo elemnento na lista
+    storeCart(); // para salvar o carrinho no localStorage - Requisito 4
   });
 }
  
-window.onload = function onload() { 
+window.onload = function onload() {
   recoverMercadoLivreResults('computador');
   addProductToCart();
+  updatePage();
 };
