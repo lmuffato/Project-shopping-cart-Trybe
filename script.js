1f9c3dbeef333ebcaad122f4d51b6ee8553f4f75
MaxRudim <max.hermsdorff@gmail.com>
2021-04-16 18:22:51 -0300

    Req: 02 CONCLUIDO

--- script.js
+++ script.js
@@ -1,43 +1,79 @@
-// window.onload = function onload() { };
+// const { identity } = require("cypress/types/lodash");
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+function cartItemClickListener(event) {
+  // coloque seu código aqui
+  return event;
+}
 
-// function createProductItemElement({ sku, name, image }) {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//   section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
 
-//   return section;
-// }
+const getItemByID = async (itemID) => {
+  const response = await fetch(`https://api.mercadolibre.com/items/${itemID}`);
+  const data = await response.json();
+  return data;
+};
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
+const getButtonsID = async (event) => {
+  const cartElements = document.querySelector('.cart__items');
+    const itemTargeted = getSkuFromProductItem(event.target.parentNode);
+    const computer = await getItemByID(itemTargeted);
+    const obj = {
+      sku: computer.id,
+      name: computer.title,
+      salePrice: computer.price,
+    };
+    cartElements.appendChild(createCartItemElement(obj));
+  };
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createProductItemElement({ sku, name, image }) {
+  const selectItems = document.querySelector('.items');
+  const section = document.createElement('section');
+  section.className = 'item';
+
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'))
+  .addEventListener('click', getButtonsID);
+  return selectItems.appendChild(section);
+}
+
+const getItem = () => {
+        fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+        .then((response) => response.json()).then((data) => data.results.forEach((pc) => {
+            const items = {
+                sku: pc.id,
+                name: pc.title,
+                image: pc.thumbnail,
+            };
+    createProductItemElement(items);
+        }));
+};
+
+window.onload = async function onload() { 
+  await getItem();
+};
