042be5ce716c286cc84db00ff72ec229381d5ba4
Marcelo Maurício <mmauricio.design@gmail.com>
2021-04-16 11:17:44 -0300

    Requisito 2 - Adiciona elementos ao carinho, selecionando features do produto a serem mostrada

--- script.js
+++ script.js
@@ -15,10 +15,7 @@ function createCustomElement(element, className, innerText) {
 async function getProductApi() { 
   return fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        return (data);
-      });
+    .then((data) => data);
 }
 
 function createProductItemElement({ sku, name, image }) {
@@ -34,36 +31,68 @@ function createProductItemElement({ sku, name, image }) {
 }
 
 async function createListProductItems() {
-const { results } = await getProductApi();
-const productsContainer = document.querySelector('.items');
-productsContainer.innerHTML = '';
-// return console.log(results);
+  const { results } = await getProductApi();
+  const productsContainer = document.querySelector('.items');
+  productsContainer.innerHTML = '';
 
-results.forEach(({ id: sku, title: name, thumbnail: image }) => {
-  productsContainer.appendChild(createProductItemElement({ sku, name, image }));
-});
+  results.forEach(({ id: sku, title: name, thumbnail: image }) => {
+    productsContainer.appendChild(createProductItemElement({ sku, name, image }));
+  });
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
 // function cartItemClickListener(event) {
-//   // coloque seu código aqui
+//   // coloque seu código aqui  
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+const getProductBySku = async (sku) => {
+ const product = fetch(`https://api.mercadolibre.com/items/${sku}`)
+    .then((response) => response.json())
+    .then((data) => data);
+  return product;
+};
 
-window.onload = function onload() { 
-  createListProductItems(); 
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const putItemInCart = (product) => {
+  const cartItensContainer = document.querySelector('.cart__items');
+
+  cartItensContainer.appendChild(createCartItemElement(product));
+};
+
+const onClick = async (event) => {
+  const buttonSelect = event.target.parentNode;
+  const idProduct = getSkuFromProductItem(buttonSelect);
+  const productSelected = await getProductBySku(idProduct);
+  const objectProduct = {
+    sku: productSelected.id,
+    name: productSelected.title,
+    salePrice: productSelected.price,
+  };
+
+  console.log(objectProduct);
+  putItemInCart(objectProduct);  
+};
+
+const addEventButtons = async () => {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => button.addEventListener('click', onClick));
+};
+
+window.onload = async function onload() { 
+  createListProductItems()
+    .then(() => addEventButtons());
 };
 
 // o window.onload foi colocado abaixo devido a ele ter que fazer as funções acima primeiro, para depois ele reconhecer e utilizá-las.
 
-// Graças ao código do [Murilo goncalves] - Project Shopping Cart #82, eu pude entender como utilizar funções async. Assim como quebrar a cabeça para entender que quando utilizado o await para uma função, a função onde ele está também deve ser uma async. Assim como só iremos conseguir maninupar o json quando a função dele retorna com o await.
+// Graças ao Murilo Goncalves, em uma call no Slack, 15/04, eu pude entender como utilizar funções async. Assim como quebrar a cabeça para entender que quando utilizado o await para uma função, a função onde ele está também deve ser uma async. Assim como só iremos conseguir maninupar o json quando a função dele retorna com o await.
