08e6abd432ad5ea6485db1c23edcb2dafc6ab86c
Elisa <elisafranca.carvalho@gmail.com>
2021-04-16 19:48:02 -0300

    Requisito 5: Soma o valor total dos itens do carrinho de forma assíncrona

--- script.js
+++ script.js
@@ -27,19 +27,39 @@ function createProductItemElement({ sku, name, image }) {
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
+const createTotalItem = () => {
+  const totalPriceParentNode = document.createElement('section');
+  totalPriceParentNode.className = 'total-price';
+  const cart = document.querySelector('.cart');
+  cart.appendChild(totalPriceParentNode);
+  const total = document.createElement('p');
+  total.className = 'shopCart_total';
+  totalPriceParentNode.appendChild(total);
+};
+
+const updateCartValue = async () => {
+  const shopCartList = document.querySelectorAll('.cart__item');
+  const total = document.querySelector('.shopCart_total');
+  let totalPrice = 0;
+  Array.from(shopCartList).forEach((item) => {
+    const itemPrice = item.dataset.price;
+    totalPrice += parseFloat(itemPrice);
+  });
+  total.innerHTML = totalPrice;
+};
 
 function cartItemClickListener(event) {
-  // Para esse trecho da função consultei a seguinte documentação:
-  // https://catalin.red/removing-an-element-with-plain-javascript-remove-method/
   const { target } = event;
   const id = target.dataset.sku;
+  // Para esse trecho da função consultei a seguinte documentação:
+  // https://catalin.red/removing-an-element-with-plain-javascript-remove-method/
   target.remove();
+  updateCartValue();
   // Para o trecho a seguir utilizando dataset, estudei o seguinte conteúdo:
   // https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLOrForeignElement/dataset
   const storageItens = JSON.parse(localStorage.getItem('cartItens'));
   if (storageItens) {
     const newArray = storageItens.filter((product) => product.sku !== id);
-    console.log(newArray);
     localStorage.setItem('cartItens', JSON.stringify(newArray));
   }
 }
@@ -56,11 +76,12 @@ function createCartItemElement({ sku, name, salePrice }) {
   // Para o trecho a seguir utilizando dataset, estudei o seguinte conteúdo:
   // https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLOrForeignElement/dataset
   li.dataset.sku = sku;
+  li.dataset.price = salePrice;
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} 
 
 const chargeLocalStorage = () => {
   const products = JSON.parse(localStorage.getItem('cartItens'));
@@ -85,11 +106,11 @@ const fetchProductItem = async (e) => {
   const endPoint = await fetch(`https://api.mercadolibre.com/items/${id}`);
   const objectJson = await endPoint.json();
   const { id: sku, title: name, price: salePrice } = objectJson;
-
   const li = createCartItemElement({ sku, name, salePrice });
   const ol = document.querySelector('.cart__items');
   ol.appendChild(li);
   addItemToLocalStorage({ sku, name, salePrice });
+  updateCartValue();
 };
 
 const handleCartAddClick = () => {
@@ -123,4 +144,6 @@ window.onload = function onload() {
   });
   verifyLocalStorage();
   chargeLocalStorage();
+  createTotalItem();
+  updateCartValue();
 };
