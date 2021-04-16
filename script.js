ea9f84d7a918030cff54b35c584f202de2130fdc
Vinicius Rodrigues <v.rodriguesjk@gmail.com>
2021-04-16 19:42:58 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -32,19 +32,19 @@ function createProductItemElement({ sku, name, image }) {
 //   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id, title, price }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 const productsFetch = () => new Promise((resolve) => {
-    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-      .then((response) => response.json()
-        .then((data) => resolve(data.results)));
-  });
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((response) => response.json()
+      .then((data) => resolve(data.results)));
+});
 
 const productAsync = async () => {
   const data = await productsFetch();
@@ -58,6 +58,26 @@ const productAsync = async () => {
   });
 };
 
+const idFetch = (id) => new Promise((resolve) => {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => response.json())
+    .then((data) => resolve(data));
+});
+
+async function getId(event) {
+  const x = createCartItemElement(await idFetch(event.target.parentNode.firstChild.innerText));
+  const cartItems = document.querySelector('.cart__items');
+  console.log(x);
+  cartItems.appendChild(x);
+}
+
+const eventClick = () => {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => {
+    button.addEventListener('click', getId);
+  });
+};
+
 window.onload = function onload() {
-  productAsync();
+  productAsync().then(() => eventClick());
 };
