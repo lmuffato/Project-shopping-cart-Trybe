198b2a0910f5a1a3b3c93d4d724ad56c699910b4
Carlos Campo <carlosgabrielcampo@gmail.com>
2021-04-15 22:12:58 -0300

    requisito 1

--- script.js
+++ script.js
@@ -1,43 +1,59 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
+const img = document.createElement('img');
+img.className = 'item__image';
+img.src = imageSource;
+return img;
 }
 
 function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
+const e = document.createElement(element);
+e.className = className;
+e.innerText = innerText;
+return e;
 }
 
 function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
-
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
-  return section;
+const section = document.createElement('section');
+section.className = 'item';
+section.appendChild(createCustomElement('span', 'item__sku', sku));
+section.appendChild(createCustomElement('span', 'item__title', name));
+section.appendChild(createProductImageElement(image));
+section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+return section;
 }
+
+window.onload = function onload() {
+  const itemAisle = document.getElementsByClassName('items')[0];
+  async function getInfo() {
+    const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
+    const info = await response.json();
+    const { results } = info;
+    results.forEach((item) => {
+      const infoExport = {
+        sku: item.id,
+        name: item.title,
+        image: item.thumbnail,
+      };
+        itemAisle.appendChild(createProductItemElement(infoExport));
+      });
+  }
+  getInfo();
+};
 
 function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+return item.querySelector('span.item__sku').innerText;
 }
+getSkuFromProductItem();
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+return event;
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
+const li = document.createElement('li');
+li.className = 'cart__item';
+li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+li.addEventListener('click', cartItemClickListener);
+return li;
 }
+createCartItemElement();
