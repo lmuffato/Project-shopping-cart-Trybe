9935ac3881f333e66cfe1231d710dca88ce2f96b
Lucas Machado <lucas.almeida.machado92@gmail.com>
2021-04-16 17:20:13 -0300

    Requisito 3 -Clicando no item do carrinho para apagar

--- script.js
+++ script.js
@@ -28,16 +28,16 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-/*
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  const getItemNoCart = event.target;
+  getItemNoCart.remove();
 }
-*/
+
 function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
- // li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartItemClickListener);
 
   return li;
 }
