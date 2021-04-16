b3c31e0f5c4d94e4a5d76cabbc77bc006db952ab
Nathalia Zebral <natzebral@gmail.com>
2021-04-16 10:32:32 -0300

    primeiro requisito - grazadeus!

--- script.js
+++ script.js
@@ -1,58 +1,63 @@
-window.onload = function onload() { };
-
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then((response) => {
-      response.json()
-        .then((data) => data.results);
-      });
-
-    // forEach(({ id, title, thumbnail }) => {
-    //   const enter = { 
-    //     sku: id, 
-    //     name: title, 
-    //     image: thumbnail,
-    //   }; 
-    //   console.log(enter);
-    // });
-
-// function createProductItemElement({ sku, name, image }) {
-//    const section = document.createElement('section');
-//    section.className = 'item';
-
-//   section.appendChild(createCustomElement('span', 'item__sku', sku));
-//    section.appendChild(createCustomElement('span', 'item__title', name));
-//   section.appendChild(createProductImageElement(image));
-//    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+// o primeiro requisitos foram feiots com ajuda dos colegas Adelino Junior , Orlando Flores,Thiago souza ,Tiago santos,Jonathan Fernandes,Nilson Ribeiro,Marília , Lucas Lara , e o Prof. Zezé e Jack !!
+function createProductImageElement(imageSource) {
+    const img = document.createElement('img');
+    img.className = 'item__image';
+    img.src = imageSource;
+    return img;
+  }
   
-// //   return section;
-// // }
+  function createCustomElement(element, className, innerText) {
+    const e = document.createElement(element);
+    e.className = className;
+    e.innerText = innerText;
+    return e;
+  }
 
-// function createProductImageElement(imageSource) {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// }
+  function createProductItemElement({ id, title, thumbnail }) {
+    const section = document.createElement('section');
+    section.className = 'item';
+    
+    section.appendChild(createCustomElement('span', 'item__sku', id));
+    section.appendChild(createCustomElement('span', 'item__title', title));
+    section.appendChild(createProductImageElement(thumbnail));
+    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+    return section;
+  }
+  
+  const productsInformation = (computerInfos) => {
+    computerInfos.forEach((computer) => {
+      const computerSection = document.querySelector('.items');
+      computerSection.appendChild(createProductItemElement(computer));
+    });
+  };
+  
+const searchComputerAPI = () => {
+    fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+      .then((response) => {
+        response.json()
+          .then((data) => {
+        const computerInfos = data.results;
+        productsInformation(computerInfos);
+      });
+      });
+    };
 
-// function createCustomElement(element, className, innerText) {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// }
+  // function getSkuFromProductItem(item) {
+    //   return item.querySelector('span.item__sku').innerText;
+    // }
+    
+    // function cartItemClickListener(event) {
+      //   // coloque seu código aqui
+      // }
+      
+      // function createCartItemElement({ id, title, salePrice }) {
+        //   const li = document.createElement('li');
+        //   li.className = 'cart__item';
+        //   li.innerText = `SKU: $ id} | NAME: ${title} | PRICE: $${salePrice}`;
+        //   li.addEventListener('click', cartItemClickListener);
+        //   return li;
+        // }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
-
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+window.onload = function onload() { 
+  searchComputerAPI();
+  };
