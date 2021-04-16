a5d060ea8182230875fcab091226f8872715c75e
Ana Ventura <anaventura1811@gmail.com>
2021-04-16 10:29:16 -0300

    Adiciona requisitos 3 e 5 e referências, corrige requisito 4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Cria a imagem de cada produto
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -5,6 +6,7 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+// Cria um elemento na página de forma customizada
 function createCustomElement(element, className, innerText) {
   const e = document.createElement(element);
   e.className = className;
@@ -12,6 +14,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// Cria o produto na página, com base nos dados extraídos da API
 function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -25,6 +28,16 @@ function createProductItemElement({ id, title, thumbnail }) {
   return section;
 }
 
+// Itera os dados e cria objeto para passar à função createProduct, melhora resolução das imagens dos produtos (dica do colega Renzo Sevilha)
+const iterandoMeusDados = (dados) => {
+  dados.forEach((dado) => {
+    const image = `https://http2.mlstatic.com/D_NQ_NP_${dado.thumbnail_id}-O.webp`;
+    const objt = { id: dado.id, title: dado.title, thumbnail: image };
+    createProductItemElement(objt);
+  });
+};
+
+// Cria o texto com classe .loading na página para aparecer antes da requisição de dados da API
 function startLoading() {
   const loading = document.createElement('h1');
   loading.classList.add('loading');
@@ -33,63 +46,85 @@ function startLoading() {
   return appendBody;
 }
 
+// Remove o elemento de classe .loading da página quando a requisição na API é feita
 const stopLoading = async () => {
   const loadingItem = document.querySelector('.loading');
   loadingItem.remove();
 };
 
+// --> Para as funções somaItensDoCarrinho e precoTotal, foram utilizadas as soluções elaboradas pelo colega Wanderson Salles
+// Soma todo os itens adicionados ao carrinho
+const somaItensDoCarrinho = () => {
+  let soma = 0;
+  const itensDoCarrinho = document.querySelectorAll('.cart__item');
+  [...itensDoCarrinho].forEach((item) => {
+    soma += parseFloat(item.innerText.substring(item.innerText.indexOf('PRICE') + 8));
+  });
+  return soma;
+};
+
+// Formata o preço total e atribui o valor ao elemento HTML com classe .total-price
+const precoTotal = () => {
+  const total = document.querySelector('.total-price');
+  total.innerHTML = (Math.round(somaItensDoCarrinho() * 100) / 100);
+};
+
+// Remove um único elemento do carrinho
+function cartItemClickListener(event) {
+  event.target.remove();
+  precoTotal();
+}
+
+// Cria um elemento li no carrinho
 function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
   const ol = document.querySelector('.cart__items');
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  li.addEventListener('click', cartItemClickListener);
   return ol.appendChild(li);
 }
-/*
-function cartItemClickListener(event) {
-  return event.target.remove();
-}
-*/
+
+// Pega o container do carrinho de compras (o elemento HTML ol)
 const fetchMyCartContainer = () => document.querySelector('ol.cart__items');
 
-/*
-const removeCartItem = () => {
-  const cartItem = fetchMyCartContainer();
-  cartItem.childNodes.forEach((child) => {
-    child.addEventListener('click', cartItemClickListener);
-  });
-};
-*/
-// Salva o carrinho no local Storage
+// Salva todo o carrinho no local Storage - função adaptada do projeto To Do List
 const saveMyCart = () => {
   const cart = fetchMyCartContainer();
-  localStorage.cart = cart.innerHTML;
+  localStorage.setItem('cart', cart.innerHTML);
 };
 
-// Recupera o carrinho do local Storage
+// Recupera o carrinho do local Storage - função adaptada do projeto To Do List
 const getMyCart = () => {
   const cart = fetchMyCartContainer();
   if (typeof Storage !== 'undefined' && localStorage.cart) {
-    cart.innerHTML = localStorage.cart;
+    cart.innerHTML = localStorage.getItem('cart');
   }
 };
 
-const addToMyCart = (data) => {
-  const btns = document.querySelectorAll('.item button');
-  btns.forEach((btn, index) => {
-    btn.addEventListener('click', () => {
-      const cartContainer = fetchMyCartContainer();
-      const addItemstoCart = createCartItemElement(data[index]);
-      cartContainer.appendChild(addItemstoCart);
-      saveMyCart();
-    });
+// Cria o evento de clique para adicionar cada item ao carrinho
+const addToMyCartClickEvent = (btn, indice, dados) => {
+  btn.addEventListener('click', () => {
+    const shoppingCart = fetchMyCartContainer();
+    const addItemstoCart = createCartItemElement(dados[indice]);
+    shoppingCart.appendChild(addItemstoCart);
+    precoTotal();
+    saveMyCart();
   });
 };
 
-// Limpa local Storage
+// Itera sobre os itens e adiciona itens ao carrinho de compras 
+const addToMyCart = (data) => {
+  const btns = document.querySelectorAll('.item button');
+  btns.forEach((btn, index) => {
+    addToMyCartClickEvent(btn, index, data);
+   });  
+};
+
+// Limpa tudo do local Storage - função adaptada do projeto To Do List
 const clearLocalStorage = () => localStorage.clear();
 
-// Remove todos os itens do carrinho
+// Remove todos os itens do carrinho - função adaptada do projeto To Do List
 function eraseAll() {
   const btnEmptyCart = document.querySelector('.empty-cart');
   btnEmptyCart.addEventListener('click', () => {
@@ -101,51 +136,28 @@ function eraseAll() {
   });
 }
 
+// Faz a requisição dos dados - com auxílio do plantão do Eliezer Queiroz e do Jackson Pires
 const productPromise = (productName) => {
   startLoading();
   let products;
+  return new Promise((resolve) => {
     fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${productName}`)
-      .then((response) => {
-        response.json().then((data) => { 
-          stopLoading();
-          products = data.results;
-          products.forEach((product) => {
-            createProductItemElement(product);
-            console.log(product);
-          });
-          addToMyCart(products);
-      });
+    .then((response) => {
+      response.json().then((data) => { 
+        stopLoading();
+        products = data.results;
+        resolve(products);
     });
-};
-// Função productPromise feita com auxílio do plantão do instrutor Eliezer 
-
-/*
-const soma = async () => {
-  const totalPrice = document.querySelector('.total-price');
-  const itensCarrinho = document.querySelectorAll('.cart__item');
-  const valorTotal = itensCarrinho.reduce((prev, value) => {
-    prev.price.split('$');
-    value.price.split('$');
-    return parseFloat(prev.price + value.price);
   });
-  totalPrice.innerText = ((Math.round(valorTotal * 100)) / 100);
-  return totalPrice;
+  });  
 };
-*/
-/*
-function getSkuFromProductItem(item) {
-  return item.querySelector('.item__sku').innerText;
-}
-*/
-/*
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
-*/
 
 const fetchProducts = async () => {
   try {
-    await productPromise('computador');
+    const data = await productPromise('computador');
+    iterandoMeusDados(data);
+    addToMyCart(data);
+    precoTotal();
   } catch (error) {
     console.log('Fail...');
   }
@@ -156,3 +168,16 @@ window.onload = function onload() {
   eraseAll();
   getMyCart();
 };
+
+// Referências:
+// Para entender como tratar os dados extraídos com o fetch, foi consultado o PR da estudante Ana Luiza Machado (turma 9):
+// -- https://github.com/tryber/sd-09-project-shopping-cart/pull/93/files
+// Para compreender a lógica do requisito 5, foi consultado o PR do colega Wanderson Salles (Turma 10 | Tribo A): 
+// -- https://github.com/tryber/sd-010-a-project-shopping-cart/pull/37/files
+// Sobre indexOf():
+// -- https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+// Sobre subtring():
+// -- https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+// Para ajustes nas funções saveMyCart e getMyCart, foram consultadas as seguintes referências:
+// -- Livro: IEPSEN, Edécio Fernando. Lógica de programação e algoritmos com JavaScript. Ed. Novatec 2018 (cap. 8 - Persistência de dados com local Storage)
+// -- Meu repositório do projeto To Do List: https://github.com/anaventura1811/minhas-tarefas
