7ec9949fb00b48216548d91e7ff47a90c1a48d5e
Villefor <franklincrius@gmail.com>
2021-04-16 01:53:06 -0300

    tentando arrumar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const cartItems = '.cart__items';
 
-function getItemsPrices() {
+function savingItems() {
   const cartLi = document.querySelector(cartItems);
   localStorage.setItem('items', cartLi.innerHTML);
 }
@@ -22,29 +22,30 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  // const addItem = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  // addItem.addEventListener('click', createCartItem);
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-}
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function cartItemClickListener(event) {
-  event.target.remove();
-  getItemsPrices();
-}
+// function cartItemClickListener(event) {
+//   event.target.remove();
+//   savingItems();
+// }
+// cartItemClickListener();
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -58,19 +59,45 @@ async function fetchProducts() {
   });
 }
 
-const createCartItem = () => {
-  const allItems = document.querySelector('.items');
-  allItems.addEventListener('click', async (event) => {
-   const idSku = getSkuFromProductItem(event.target.parentNode);
-   const getItems = await fetch(`https://api.mercadolibre.com/items/${idSku}`)
-    .then((result) => result.json());
-   const getPrice = { sku: idSku, name: getItems.title, salePrice: getItems.price };
-   const cartLoad = createCartItemElement(getPrice);
-   document.querySelector('ol.cart__items').appendChild(cartLoad);
-   getItemsPrices();
- });
+// const createCartItem = () => {
+//   const everyItem = document.querySelector('.items');
+//   everyItem.addEventListener('click', async (event) => {
+//    const idSku = getSkuFromProductItem(event.target.parentElement);
+//    const getItems = await fetch(`https://api.mercadolibre.com/items/${idSku}`)
+//     .then((result) => result.json());
+//    const getPrice = { sku: idSku, name: getItems.title, salePrice: getItems.price };
+//    const cartLoad = createCartItemElement(getPrice);
+//    document.querySelector('ol.cart__items').appendChild(cartLoad);
+//    savingItems();
+//  });
+// };
+
+// tentando de outra forma, já que o parent não estava funcionando.
+async function createCartItem(sku) {
+  const olList = document.querySelector(cartItems);
+  const idToSku = sku;
+  const result = await fetch(`https://api.mercadolibre.com/items/${idToSku}`);
+  const json = await result.json()
+  .then((item) => createCartItemElement({
+        sku: item.id,
+        name: item.title,
+        salePrice: item.price,
+   }));
+  olList.appendChild(json);
+  savingItems();
+}
+
+// referência Patrick, turma A. Me deu noção de como fazer o botão de maneira pragmática.
+const clearingCart = () => {
+  const clearButton = document.querySelector('.empty-cart');
+  clearButton.addEventListener('click', () => {
+    const clearingItems = document.querySelector(cartItems);
+    clearingItems.innerHTML = '';
+    savingItems();
+  });
 };
 
 window.onload = function onload() { };
 fetchProducts();
 createCartItem();
+clearingCart();
