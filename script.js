8fc6b0af57e9fb07e2746af2f9e61f58c7ffa49c
Eduardo Seije <eduardoseije@gmail.com>
2021-04-17 17:10:02 -0300

    Requisito 01- retirando o new promise

--- script.js
+++ script.js
@@ -14,48 +14,43 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id, title, thumbnail }) {
   const section = document.createElement('section');
   section.className = 'item';
   const sectionItems = document.querySelector('.items');
   sectionItems.appendChild(section);
   
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('span', 'item__sku', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
   
   return section;
 }
 
-const fetchPcs = () => new Promise((resolve) => {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
-    .then((response) => {
-      response.json()
-      .then((data) => {
-        resolve(data.results);
+const fetchProducts = () => {
+  const endpointpcs = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+  const promise = fetch(endpointpcs);
+  promise
+  .then((response) => {
+    response.json()
+    .then((pcData) => {
+      pcData.results.forEach((element) => {
+        createProductItemElement(element);
       });
     });
-});
-
-const addProducts = async () => {
-  const pcList = await fetchPcs();
-  pcList.forEach((pc) => {
-    const pcData = [{
-    sku: pc.id,
-    name: pc.title,
-    image: pc.thumbnail,         
-    }];
-    pcData.forEach((pcItem) => createProductItemElement(pcItem));
   });
 };
 
 // function getSkuFromProductItem(item) {
-  //         return item.querySelector('span.item__sku').innerText;
-  // }
+//   return item.querySelector('span.item__sku').innerText;
+// }
   
-  // function cartItemClickListener(event) {}
+// function cartItemClickListener(event) {
   
+// }
+// // cartItemClickListener();
+
 // function createCartItemElement({ sku, name, salePrice }) {
 //   const li = document.createElement('li');
 //     li.className = 'cart__item';
@@ -63,5 +58,5 @@ const addProducts = async () => {
 //     li.addEventListener('click', cartItemClickListener);
 //     return li;
 // }
-    
-window.onload = function onload() { addProducts(); };
+
+window.onload = function onload() { fetchProducts(); };
