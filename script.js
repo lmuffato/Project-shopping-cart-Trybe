5af58026f2f1ba997fc52da1e2f0750bb6a915d1
guilhermemd <guilherme.md@gmail.com>
2021-04-16 22:00:50 -0300

    requisito 002

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+function cartItemClickListener() {
   // coloque seu código aqui
 }
 
