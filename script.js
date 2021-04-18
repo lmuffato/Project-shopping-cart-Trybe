9f8ee095e7e7266ce654596a8151d0cc5796dfcd
André Jaques Batista <andrejaques.b@gmail.com>
2021-04-18 14:27:56 -0300

    add require 01 - Tudo por 50% do dobro

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
-/* window.onload = function onload() { }; */
+/* const fetch = require('node-fetch'); */
 
-/* function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-} */
+async function getData(QUERY) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data.results;
+}
 
-/* function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
-  return e;
-} */
+async function addDataList(QUERY) {
+  const productList = await getData(QUERY)
+  const marketSection = document.querySelector('section.items')
 
-/* function createProductItemElement({ sku, name, image }) {
+  productList.forEach((product) => {
+    const item = createProductItemElement({
+      sku: product.id,
+      name: product.title,
+      image: product.thumbnail,
+      price: toReal(product.price),
+    });
+
+    marketSection.appendChild(item);
+  })
+}
+
+function toReal(n) {
+  return "R$ " + n.toFixed(2).replace('.', ',').replace(/(\d)(?=(\d{3})+\,)/g, "$1.");
+}
+
+function createProductItemElement({ sku: id, name, image, price }) {
   const section = document.createElement('section');
   section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__sku', id));
   section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createCustomElement('span', 'item__title', price));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-} */
+}
+
+function createCustomElement(element, className, innerText) {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+}
+
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+}
 
 /* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -41,3 +69,5 @@
   li.addEventListener('click', cartItemClickListener);
   return li;
 } */
+
+addDataList('computador');
