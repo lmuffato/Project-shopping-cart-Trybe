fa01e2361bebd2d2110b916ffb3a56a94b3462ab
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-04-18 06:04:11 -0300

    requisito 2, testes

--- script.js
+++ script.js
@@ -24,21 +24,47 @@ function createProductItemElement({ id, title, thumbnail }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id, title, price }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${id} | NAME: ${title} | PRICE: $${price}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+// x ---------------requisito 2 abaixo -----------------------x
+
+const procuraId = (id) => {
+  fetch(`https://api.mercadolibre.com/items/${id}`)
+  .then((response) => {
+    response.json()
+    .then((data) => {
+      const idinfo = data;
+      createCartItemElement(idinfo);
+      document.querySelector('.cart__items').appendChild(createCartItemElement(idinfo));
+    });
+  });
+};
+
+const pegaDadosID = () => {
+  const caminho = document.querySelectorAll('.item__add');
+  caminho.forEach((button) => {
+    button.addEventListener('click', (event) => {
+      const idDoPc = getSkuFromProductItem(event.target.parentElement);
+      procuraId(idDoPc);
+    });
+  });
+};
+
+// x------------------requisito 1 abaixo---------------------x
 
 const informaçoesApi = (interageApi) => {
   interageApi.forEach((computer) => { 
@@ -56,7 +82,9 @@ const interageApi = () => {
     });
   });
  };
-// consegui fazer o requisito 1 com ajuda do Adelino Junior, Tiago santos, tiago souza,Nathi zebral, Lucas lara, Marilia e os instrutores da trybe.
+
+// consegui fazer o requisito 1 com ajuda do Adelino Junior, Tiago santos, tiago souza,Nathi zebral, Lucas lara, Marilia e os instrutores.
  window.onload = function onload() { 
    interageApi();
+   pegaDadosID();
 };
