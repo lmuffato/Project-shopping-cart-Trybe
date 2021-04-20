a301ddc9aee2c86b46b015447a6647d8237b5f22
carlos25xrm <carlos25xrm@gmail.com>
2021-04-20 15:32:50 -0300

    Requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
-/* function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
-} */
+// #10 - Ao clicar no item do carrinho de compras, verifica se o item clicado
+// possui a classe "cart__item", se tiver remove o conteúdo.
+function cartItemClickListener(event) {
+  const { target } = event;
+  if (target.classList.contains('cart__item')) {
+    target.remove('li');
+  }
+}
 
-/* function cartItemClickListener(event) {
-  // coloque seu código aqui
-} */
-
-/* function createCartItemElement({ sku, name, salePrice }) {
+// #9 - Recebe as informações de um único produto e coloca no carrinho de compras
+// informando apenas o ID, descrição e preço (Achei melhor utilizar esses termos). 
+function createCartItemElement({ id, title, price }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `ID: ${id} \n DESCRIÇÃO: ${title} \n PREÇO: $${price}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
 
 // #1 - Função que acessa a API do Mercado Livre buscando pelo termo "computador".
-async function acessarAPI() {
+const acessarAPI = async () => {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
   const data = await response.json();
   return data.results;
-}
-
-// # 5 - Função que pega a imagem enviada pela função "createProductItemElement",
-// cria uma TAG HTML do tipo "img", coloca essa TAG com a classe "item__image",
-// indica a fonte da imagem (Parâmetro dessa mesma função) e retorna a imagem.
-function createProductImageElement(imageSource) {
-  const img = document.createElement('img');
-  img.className = 'item__image';
-  img.src = imageSource;
-  return img;
-}
+};
 
 // #4 - Função utilizada para criar um novo elemento com uma identificação, classe
 // e texto baseado nas informações enviadas pela função "createProductItemElement".
@@ -38,6 +31,16 @@ function createCustomElement(element, className, innerText) {
   e.className = className;
   e.innerText = innerText;
   return e;
+}
+
+// #5 - Função que pega a imagem enviada pela função "createProductItemElement",
+// cria uma TAG HTML do tipo "img", coloca essa TAG com a classe "item__image",
+// indica a fonte da imagem (Parâmetro dessa mesma função) e retorna a imagem.
+function createProductImageElement(imageSource) {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
 }
 
 // #3 - Preciei mudar "sku" para "id", "name" para "title" e "image" para "thumbnail",
@@ -56,17 +59,43 @@ function createProductItemElement({ id, title, thumbnail }) {
   return section;
 }
 
-// # 2 - Função que pega a busca feita na API do Mercado Livre e envia
+// #2 - Função que pega a busca feita na API do Mercado Livre e envia
 // como parâmetro da função "createProductItemElement".
-async function resultadosAPI() {
+const resultadosAPI = async () => {
   const computadores = await acessarAPI();
   computadores.forEach((computador) => {
     const cadaItem = document.querySelector('.items');
     cadaItem.appendChild(createProductItemElement(computador)); 
   });
+};
+
+// #8 - Faz uma nova busca na API do Mercado Livre através do ID (Sku) do produto.
+// Depois pega a informação recebida e envia para a função "createCartItemElement".
+async function buscarItemNaAPI(ItemID) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${ItemID}`);
+  const data = await response.json();
+  createCartItemElement(data);
+  document.querySelector('.cart__items').appendChild(createCartItemElement(data));
 }
 
-window.onload = function () {
+// #7 - Identifica e retorna o ID (Sku) do produto para a "const computadorID".
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
+
+// #6 - Identifica o click no botão do anúncio e envia
+// o "target" para a função "getSkuFromProductItem".
+const clickButton = () => {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((botao) => {
+    botao.addEventListener('click', (event) => {
+      const computadorID = getSkuFromProductItem(event.target.parentElement);
+      buscarItemNaAPI(computadorID);
+    });
+  });  
+};
+
+window.onload = function onload() {
   acessarAPI();
-  resultadosAPI();
+  resultadosAPI().then(() => clickButton());
 };
