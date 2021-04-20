aaf0f52fa6c116fbb644880fd38119d8b34968d1
Debora Passos <deborinhapassos@gmail.com>
2021-04-20 16:57:03 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-window.onload = function onload() { };
+
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -14,6 +14,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
+// REQUISITO 1 (USAR)
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
@@ -26,22 +27,42 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+// REQUISITO 2 (USAR)
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
 // function cartItemClickListener(event) {
 //   // coloque seu código aqui
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+// REQUISITO 2 (USAR)
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  // li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
+// REQUISITO 2 (MONTAR)
+const getEndPoint = (itemID) => fetch(`https://api.mercadolibre.com/items/${itemID}`)
+   .then((response) => response.json());
+  
+  const cartItemClickListener = async (event) => {
+  const itens = getSkuFromProductItem(event.target.parentNode);
+  const elements = await getEndPoint(itens);
+  const arrObj = { sku: elements.id, name: elements.title, salePrice: elements.price };
+  // até aqui captura os elementos que vão no objeto 
+  
+  const insertInCart = createCartItemElement(arrObj);
+  document.querySelector('.cart__items').appendChild(insertInCart);
+};
+  const creatAddList = () => {
+  document.querySelector('.items').addEventListener('click', cartItemClickListener);
+};
+
+// REQUISITO 1 (MONTAR)
 async function getInfo() {
   const elementListItem = document.querySelector('.items');
   const products = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador');
@@ -50,10 +71,13 @@ async function getInfo() {
 
   res.forEach((infoItem) => {
     const listItem = { sku: infoItem.id, name: infoItem.title, image: infoItem.thumbnail };
-    
+
     // console.log(listItem);
     elementListItem.appendChild(createProductItemElement(listItem));
   }); 
 }
 
+window.onload = function onload() {
 getInfo();
+creatAddList();
+};
