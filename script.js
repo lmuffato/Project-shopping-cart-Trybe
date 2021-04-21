bcc1ae5b2ee367463f0fd297ec60827af2c6f909
Luiz Putinatti <luiz_edu16@hotmail.com>
2021-04-21 18:40:05 -0300

    inclusao do requisito 2

--- script.js
+++ script.js
@@ -38,22 +38,43 @@ const fetchProduto = () => {
   });
 };
 
-window.onload = function onload() { 
-  fetchProduto();
-};
-
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
 // function cartItemClickListener(event) {
-//   // coloque seu código aqui
+
 // }
 
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ sku, name, salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
+
+const buscaItem = (event) => {
+  const eventProdut = getSkuFromProductItem(event.target);
+  fetch(`https://api.mercadolibre.com/items/${eventProdut}`)
+  .then((response) => {
+    response.json().then((data) => {
+      const produtCar = {
+        sku: data.id,
+        name: data.title,
+        salePrice: data.price,
+      };
+      document.querySelector('.cart__items').appendChild(createCartItemElement(produtCar));
+    });
+  });
+}
+
+const clickAddCar = () => {
+  const buttonEvent = document.querySelectorAll('.item__add');
+  buttonEvent.forEach((button) => button.addEventListener('click', buscaItem));
+};
+
+window.onload = function onload () { 
+  fetchProduto();
+  clickAddCar();
+};
