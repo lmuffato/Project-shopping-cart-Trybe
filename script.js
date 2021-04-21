c1242033125bf447d679635557581f309239c925
Douglas <douguinhodns@gmail.com>
2021-04-21 16:44:46 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const getProduct = () => {
+const getComputer = () => { // requisito 1
   const myPromisse = new Promise((resolve) => {
     fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
       .then((response) => {
@@ -8,6 +8,15 @@ const getProduct = () => {
       });
   });
   return myPromisse;
+};
+
+const getItem = async (idItem) => { // requisito 2
+  const url = `https://api.mercadolibre.com/items/${idItem}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  const { id, title, price } = data;
+  const result = { sku: id, name: title, salePrice: price }
+  return result;
 };
 
 function createProductImageElement(imageSource) { // requisito 1 ajuda do Eduardo Costa e Andy
@@ -36,31 +45,53 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) { 
   return section;
 }
 
-/* function getSkuFromProductItem(item) {
+function getSkuFromProductItem(item) { // requisito 2
   return item.querySelector('span.item__sku').innerText;
-} */
+}
 
-/* function cartItemClickListener(event) {
+/* function cartItemClickListener(event) { // requisito 3
   // coloque seu código aqui
 } */
 
-/* function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ sku, name, salePrice }) { // requiito 2
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
 
 const addElement = async () => { // requisito 1 ajuda do Eduardo Costa e Andy
-  const waitGetProduct = await getProduct();
-  waitGetProduct.forEach((item) => {
+  const waitGetComputer = await getComputer();
+  waitGetComputer.forEach((item) => {
     const firstSection = document.querySelector('.items');
     firstSection.appendChild(createProductItemElement(item));
   });
+  eventButt();
+};
+
+const eventButt = () => { // requisito 2
+  const buttons =  document.querySelectorAll('.item__add');
+  buttons.forEach((butt) => butt.addEventListener('click', async (event) => {
+    const eventItem = event.target;
+    const skuId = getSkuFromProductItem(eventItem.parentElement);
+    const dataProduct = await getItem(skuId);
+    const cardItems = document.querySelector('.cart__items');
+    cardItems.appendChild(createCartItemElement(dataProduct));
+  }));
 };
 
 window.onload = function onload() {
-  getProduct();
+  getComputer();
   addElement();
 };
+
+/* const getItem = (idItem) => {
+  fetch(`https://api.mercadolibre.com/items/${idItem}`)
+      .then((response) => {
+        response.json().then((data) => {
+          console.log(data);
+        });
+      });
+}
+getItem('MLB1341706310') */
