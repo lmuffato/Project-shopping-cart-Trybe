396d585cdcbe33383e8ddce8808c9a1d4c188df0
Priscila <prihscapin@gmail.com>
2021-04-20 22:48:13 -0300

    Requisito 1

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-window.onload = function onload() { };
-
+// Agradecimento especial ao Rafael Dorneles - T10 - Tribo A- por todo incentivo e auxílio neste projeto. :)
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -17,7 +16,7 @@ function createCustomElement(element, className, innerText) {
 function createProductItemElement({ sku, name, image }) {
   const section = document.createElement('section');
   section.className = 'item';
-
+ 
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
@@ -26,21 +25,39 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
-  return item.querySelector('span.item__sku').innerText;
+// Requisito 1:
+function criaLista() {
+  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
+    .then((response) => response.json())
+    .then((data) => data.results.forEach((produto) => {
+      const prodObj = {
+        sku: produto.id,
+        name: produto.title,
+        image: produto.thumbnail,
+      };
+      document.querySelector('.items').appendChild(createProductItemElement(prodObj));
+    }));
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
-}
+window.onload = function onload() {
+  criaLista();
+};
+// --------------------------------------------------------------------------------------------
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
+// }
 
-function createCartItemElement({ sku, name, salePrice }) {
-  const li = document.createElement('li');
-  li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
-  return li;
-}
+// function cartItemClickListener(event) {
+//   // coloque seu código aqui
+// }
+
+// function createCartItemElement({ sku, name, salePrice }) {
+//   const li = document.createElement('li');
+//   li.className = 'cart__item';
+//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
 // Adicione o elemento retornado da função createProductItemElement(product) como filho do elemento <section class="items">.
 
