6928e4c7e42818e8c13a3661d1b586e424019528
Maria Lisboa <marialisboa1515@gmail.com>
2021-04-21 14:06:53 -0300

    refiz novamnte o 7

--- script.js
+++ script.js
@@ -34,10 +34,11 @@ const createList = (data) => {
 const fetchDataList = () => new Promise((resolve, reject) => {  
     fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador')
     .then((response) => {
+      document.getElementsByClassName('loading')[0].innerText = 'loading...';
       response.json().then((data) => {
         resolve(data.results);
         console.log(data.results);
-        document.getElementsByClassName('loading')[0].remove();
+        document.getElementsByClassName('loading')[0].innerText = '';
       })
         .catch((erro) => {
           reject(erro);
