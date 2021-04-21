e96f373b8da9eb3c7f0808a1291951dea22916c2
Douglas <douguinhodns@gmail.com>
2021-04-21 13:46:17 -0300

    requisito 1 iniciando o projeto. VAMOS COM TUDO :rocket:

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-window.onload = function onload() { };
+const getProduct = () => {
+  const myPromisse = new Promise((resolve) => {
+    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=$computador`)
+      .then((response) => {
+        response.json().then((data) => {
+          resolve(data.results);
+        })
+      })
+  })
+  return myPromisse;
+};
 
-function createProductImageElement(imageSource) {
+function createProductImageElement(imageSource) { // requisito 1 ajuda do Eduardo Costa e Andy
   const img = document.createElement('img');
   img.className = 'item__image';
   img.src = imageSource;
   return img;
 }
 
-function createCustomElement(element, className, innerText) {
+function createCustomElement(element, className, innerText) { // requisito 1 ajuda do Eduardo Costa e Andy
   const e = document.createElement(element);
   e.className = className;
   e.innerText = innerText;
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) { // requisito 1 ajuda do Eduardo Costa e Andy
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -26,18 +36,31 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-}
+} */
 
-function cartItemClickListener(event) {
+/* function cartItemClickListener(event) {
   // coloque seu código aqui
-}
+} */
 
-function createCartItemElement({ sku, name, salePrice }) {
+/* function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
+} */
+
+const addElement = async () => { // requisito 1 ajuda do Eduardo Costa e Andy
+  const waitGetProduct = await getProduct();
+  waitGetProduct.forEach((item) => {
+    const firstSection = document.querySelector('.items');
+    firstSection.appendChild(createProductItemElement(item));
+  });
+};
+
+window.onload = function onload() {
+  getProduct();
+  addElement();
+};
