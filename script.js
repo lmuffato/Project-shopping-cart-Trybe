be802d4eb9e04d1ff8a4d0014c99d567083f2f6a
William <w.willers@outlook.com>
2021-04-21 22:15:14 -0400

    Ultimo requisito

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const cartItems = document.querySelector('.cart__items');
 
-function transformKeys(arrayDeObjs) {
-  const arrays = arrayDeObjs.map((produto) => {
-    const { id: sku, title: name, price: salePrice, thumbnail: image } = produto;
+function transformKeys(productArray) {
+  const arrays = productArray.map((produto) => {
+    const {
+      id: sku,
+      title: name,
+      price: salePrice,
+      thumbnail: image,
+    } = produto;
     return { sku, name, salePrice, image };
   });
   return arrays;
@@ -27,10 +32,33 @@ function setLocalStorage() {
   localStorage.setItem('userSession', listaProdutos);
 }
 
+function fetchCartItemsValue() {
+  const prices = [];
+  cartItems.childNodes.forEach((item) => {
+    const text = item.innerHTML;
+    const split = text.split(':');
+    const price = split[split.length - 1].trim().replace('$', '');
+    return prices.push(price);
+  });
+  const toNumber = prices.map((item) => Number(item));
+  if (toNumber.length <= 1) {
+    return toNumber;
+  }
+  const totalPrice = toNumber.reduce((acc, currVal) => acc + currVal);
+  return totalPrice;
+}
+
+async function sumCartItems() {
+  const displayPrice = document.body.querySelector('.total-price');
+  const totalPrice = fetchCartItemsValue();
+  displayPrice.innerHTML = totalPrice;
+}
+
 function cartItemClickListener(event) {
   cartItems.removeChild(event.target);
   localStorage.removeItem('userSession');
   setLocalStorage();
+  sumCartItems();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -47,6 +75,7 @@ async function addToCart(id) {
   const parseKeys = transformKeys([responseToJson]);
   cartItems.appendChild(createCartItemElement(parseKeys[0]));
   setLocalStorage();
+  sumCartItems();
 }
 
 function createProductItemElement({ sku, name, image }) {
@@ -56,29 +85,35 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__sku', sku));
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
-  const button = createCustomElement('button', 'item__add', 'Adicionar ao carrinho!');
+  const button = createCustomElement(
+    'button',
+    'item__add',
+    'Adicionar ao carrinho!',
+  );
   button.addEventListener('click', () => {
     addToCart(sku);
   });
   section.appendChild(button);
 
   return section;
-} 
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
 function insertItems(produtos) {
-  const sessao = document.querySelector('.items');
+  const itemSession = document.querySelector('.items');
   produtos.forEach((item) => {
     const insertElement = createProductItemElement(item);
-    sessao.appendChild(insertElement);
+    itemSession.appendChild(insertElement);
   });
 }
 
 async function getProductsByName() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const response = await fetch(
+    'https://api.mercadolibre.com/sites/MLB/search?q=computador',
+  );
   const responseToJson = await response.json();
   const { results } = responseToJson;
   const produtos = transformKeys(results);
@@ -89,13 +124,15 @@ function getUserSession() {
   const content = localStorage.getItem('userSession');
   cartItems.innerHTML = content;
   const items = document.querySelectorAll('.cart__item');
-  items.forEach((item) => item.addEventListener('click', cartItemClickListener));
+  items.forEach((item) =>
+    item.addEventListener('click', cartItemClickListener));
 }
 
 document.querySelector('.empty-cart').addEventListener('click', () => {
   const items = document.querySelectorAll('.cart__item');
   items.forEach((item) => item.remove());
   localStorage.removeItem('userSession');
+  sumCartItems();
 });
 
 setTimeout(() => {
@@ -105,4 +142,5 @@ setTimeout(() => {
 window.onload = function onload() {
   getProductsByName();
   getUserSession();
- };
+  sumCartItems();
+};
