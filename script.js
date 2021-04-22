5b33cba13c0157cf5018b59ac6ad9037ef0fc837
Roberto Natividade <robertonnv@gmail.com>
2021-04-22 11:39:56 -0300

    Requisito

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ function cartItemClickListener(event) {
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
-  li.className = 'cart_item'
+  li.className = 'cart_item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
