ceadb2d8f9e661873a278b0f36ffbc52682e64ca
Roberto Natividade <robertonnv@gmail.com>
2021-04-22 00:41:54 -0300

    Requisito

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 const fetchMercadoLivre = () => {
+  const api_Url = 'https://api.mercadolibre.com/sites/MLB/search?q=$computador';
   return new Promise((resolve) => {
-    fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+    fetch(api_Url)
     .then((response) => {
       response.json().then((computado) => {
         resolve(computado);
       });
     });
+  });
+};
+
+const buscarItem = (idI) => {
+  const endpointItem = `https://api.mercadolibre.com/items/${idI}`;
+  return new Promise((resolve) => {
+    fetch(endpointItem)
+      .then((response) => {
+        response.json().then((item) => {
+          resolve(item);
+        });
+      });
   });
 };
 
@@ -33,25 +46,74 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
   return section;
-  return item.querySelector('span.item_sku').innerText;
 }
-/*
+
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
+const cartItemClickListener = (event) => {
   // coloque seu código aqui
-}
+  const eventListItem = event.target;
+  eventListItem.remove();
+};
 
-function createCartItemElement({ sku, name, salePrice }) {
+const createCartItemElement = ({ id:sku, title:name, price:salePrice }) => {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-*/
+};
+
+const addItem = () => {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => button.addEventListener('click', async (event) => {
+    const addEvent = event.target;
+    const skulId = getSkuFromProductItem(addEvent.parentElement);
+    const data = await fetchItem(skulId);
+    const itemsCart = document.querySelector('.cart__items');
+    itemsCart.appendChild(createCartItemElement(data));
+  }));
+};
+
+const clear = () => { // requisito 6
+  const buttonClear = document.querySelector('.empty-cart');
+  buttonClear.addEventListener('click', () => {
+    const itemsCart = document.querySelector('.cart__items');
+    itemsCart.innerHTML = '';
+  });
+};
+
+// Ajuda de Murilo Gonçalves - Turma 10 - Tribo A
+const msgLoading = () => { // requisito 7
+  const loading = document.querySelector('.loading');
+  loading.remove();
+};
+
+const createProductItemElement = async () => { // requisito 1
+  const computers = await fetchAPI();
+  msgLoading();
+
+  computers.forEach(({ id, title, thumbnail }) => {
+    const section = document.createElement('section');
+    const sectionItems = document.querySelector('.items');
+    section.className = 'item';
+
+    section.appendChild(createCustomElement('span', 'item__sku', id));
+    section.appendChild(createCustomElement('span', 'item__title', title));
+    section.appendChild(createProductImageElement(thumbnail));
+    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+
+    sectionItems.appendChild(section);
+  });
+
+  adItem();
+  clear();
+};
+
+
+
 window.onload = function onload() {
   fetchMercadoLivre();
   createProductItemElement();
