5013603e7c0f951f4963a57b3494c3b8821165c9
Robertson <robertson.nunes@gmail.com>
2021-04-21 23:19:03 -0300

    Correçoes Requisito 2

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function cartItemClickListener(event) {
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICEgit g: $${salePrice}`;
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
