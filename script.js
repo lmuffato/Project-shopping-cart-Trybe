092a31a8b5140623d1c48def49831a4811ff0417
Roberto Natividade <robertonnv@gmail.com>
2021-04-22 22:57:19 -0300

    Requisito

--- script.js
+++ script.js
@@ -18,27 +18,27 @@
     section.appendChild(createCustomElement('span', 'item__sku', id));
     section.appendChild(createCustomElement('span', 'item__title', title));
     section.appendChild(createProductImageElement(thumbnail));
-    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!')) ;
+    section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
     return section;
   } 
  // 2 passo
   function carregaPagina(resultado) {
     const recebArrayResults = resultado.results;
     recebArrayResults.forEach((element) => {
-      const item = createProductItemElement(element) // criando o elemento
+      const item = createProductItemElement(element); // criando o elemento
       const section = document.querySelector('.items');
-      section.appendChild(item)
+      section.appendChild(item);
     });
-  };
+  }
   
   function fetchMercadoLivre() {
    return new Promise((resolve) => {
-     fetch("https://api.mercadolibre.com/sites/MLB/search?q=$computador")
+     fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
        .then((response) => {
          response.json().then((data) => resolve(data));
        });
    }); 
-  }; //1 passo
+  } // 1 passo
 
   /* async function inicioPagina() {
     const dados = await fetchMercadoLivre(); // tem todos os dados da API
@@ -49,5 +49,4 @@
     // inicioPagina();
     const dados = await fetchMercadoLivre(); // tem todos os dados da API, await aguarda o retorno da promise
     carregaPagina(dados); 
-  }
-  
+  };
