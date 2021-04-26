3b34a2e6eb38b10d294b4c5fbec39db2f6c43dc6
Vinicios <soicinivt@gmail.com>
2021-04-26 15:45:39 -0300

    Requisito 2 concluido antes de ficar maluco :D

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -35,14 +33,32 @@ const verifiedFetch = async (url) => {
   throw new Error('endpoint não existe');
 };
 
+const asd = async (selectedId) => {
+  const cartItem = document.querySelector('.cart__items');
+  const product = await fetch(`https://api.mercadolibre.com/items/${selectedId}`)
+    .then((r) => r.json())
+    .then((r) => r);
+  cartItem.appendChild(createCartItemElement(product));
+};
+
+const sla = async () => {
+  const a = document.querySelectorAll('.item__add');
+  let selectedId;
+  a.forEach((element) => element.addEventListener('click', (e) => {
+    const id = e.target.previousElementSibling.previousElementSibling
+    .previousElementSibling.innerText;
+    selectedId = id;
+    asd(selectedId);
+  }));
+};
+
 const execute = async () => {
   const feti = await verifiedFetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const result = feti.results;
   const items = document.querySelector('.items'); 
   result.forEach((element) => items.appendChild(createProductItemElement(element)));
+  sla();
 };
-
-execute();
 
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
@@ -52,10 +68,14 @@ function cartItemClickListener(event) {
   // coloque seu código aqui
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+window.onload = function onload() {
+  execute();
+};
