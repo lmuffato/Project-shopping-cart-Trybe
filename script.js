2e11ee77f71d5ad32f826682c697fe8bf15713a4
Alan Albuquerque <alan.alb.flopes@gmail.com>
2021-05-03 15:17:27 -0400

    add req[5].fix[3]

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function sumPrices() {
       .split('$');
       const price = el[1];
       totalPrice += parseFloat(price);
-      addOnPriceElement(Math.round(totalPrice * 100) / 100);
+      addOnPriceElement(totalPrice);
     });
   } 
   });
