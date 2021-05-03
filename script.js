65d63dbc53e295fca7e2ba91a7715281c5577e28
Victor Ricco <victor.ricco001@gmail.com>
2021-05-03 18:11:46 -0300

    oloco

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-window.onload = function onload() { };
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -30,8 +28,11 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-function cartItemClickListener(event) {
-  // coloque seu código aqui
+function cartItemClickListener() {
+  const cartItems = document.querySelector('.cart__items');
+  cartItems.addEventListener('click', function (event) {
+    event.target.remove();
+  });
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -41,3 +42,68 @@ function createCartItemElement({ sku, name, salePrice }) {
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
+
+// MY CODE
+
+const fetchApiElements = (search) => {
+  return new Promise ((resolve) => {
+    fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`)
+    .then((response) => {
+      response.json().then((data) => {
+        const result = data.results;
+        resolve(result);
+      });
+    });
+  });
+}
+
+const fetchItemInfos = id => {
+  return new Promise ((resolve) => {
+    fetch(`https://api.mercadolibre.com/items/${id}`)
+    .then((response) => {
+      response.json().then((data) => {
+        const result = data;
+        resolve(result);
+      });
+    });
+  });
+}
+
+function tookElements() {
+  fetchApiElements('computador')
+  .then((result) => {
+    result.forEach(current => {
+      const { id: sku, title: name, thumbnail: image } = current;
+      const elementFrame = createProductItemElement({ sku, name, image });
+      const itemLocation = document.querySelector('.items');
+      itemLocation.appendChild(elementFrame);
+    });
+  });
+}
+
+function fetchElementId(item) {
+  fetchItemInfos(item)
+    .then((result) => {
+      const { id: sku, title: name, base_price: salePrice} = result
+      const cartElementFrame = createCartItemElement({sku, name, salePrice})
+      const itemLocation = document.querySelector('.cart__items')
+      itemLocation.appendChild(cartElementFrame)
+    });
+};
+
+function addToCart () {
+  const items = document.querySelectorAll('.items');
+  console.log(items)
+  items.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      const itemId = getSkuFromProductItem(event.target.parentNode);
+      console.log(event.target.parentNode)
+      fetchElementId(itemId);
+    });
+  });
+}
+
+window.onload = function onload() {
+  tookElements();
+  addToCart();
+};
