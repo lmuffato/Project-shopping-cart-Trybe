54a804d72846e5b4665fcc31f6b2ecca63a15413
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-06 13:56:49 -0300

    Resolvendo a promisse

--- script.js
+++ script.js
@@ -20,16 +20,16 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
-  const section = document.createElement('section');
-  section.className = 'item';
+// function createProductItemElement({ sku, name, image }) {
+  // const section = document.createElement('section');
+  // section.className = 'item';
 
-  section.appendChild(createCustomElement('span', 'item__sku', sku));
-  section.appendChild(createCustomElement('span', 'item__title', name));
-  section.appendChild(createProductImageElement(image));
-  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  // section.appendChild(createCustomElement('span', 'item__sku', sku));
+  // section.appendChild(createCustomElement('span', 'item__title', name));
+  // section.appendChild(createProductImageElement(image));
+  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  return section;
+  // return section;
 }
 
 // function getSkuFromProductItem(item) {
