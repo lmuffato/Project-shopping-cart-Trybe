4f1704fb7e0e2b572669d8fd95d6520523c2b870
Luiz <luizfvaccarif@gmail.com>
2021-05-06 23:50:15 +0100

    Requirement 2

--- script.js
+++ script.js
@@ -24,22 +24,21 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return section;
 }
 
- /*  function getSkuFromProductItem(item) {
+  function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
-} */
+} 
 
-/* function cartItemClickListener(event) {
+  function cartItemClickListener(/* event */) {
   // coloque seu código aqui
-}
- */
-
-/* function createCartItemElement({ sku, name, salePrice }) {
+ }
+ 
+  function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-} */
+}
 
 // R1
 
@@ -60,6 +59,35 @@ const getData = () => {
   });
 };
 
+// R2
+
+const showCart = (productById) => {
+  const getCartItems = document.querySelector('.cart__items');
+  getCartItems.appendChild(createCartItemElement(productById));
+  };
+
+const getProductById = () => {
+  const itemsClass = document.querySelector('.items');
+  itemsClass.addEventListener('click', (event) => {
+    if (event.target.classList.contains('item__add')) {
+      const { parentElement } = event.target; // destructuring solicitado pelo lint
+      const id = getSkuFromProductItem(parentElement);
+      fetch(`https://api.mercadolibre.com/items/${id}`)
+      .then((response) => {
+        response.json()
+        .then((productById) => {
+          showCart(productById);
+        });
+      });
+    }
+  });
+};
+  
 window.onload = function onload() { 
   getData();
+  getProductById();
 };
+
+// Requisitos 1 e 2 realizados com a ajuda dos vídeos:
+// https://app.betrybe.com/course/live-lectures/sd-cohort-10-a#content-review-bloco-9-dia-93
+// https://trybecourse.slack.com/files/U01AYN59Y2J/F01HW4TF02U/1-4.mp4
