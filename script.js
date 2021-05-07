5a9db9b4925025ac9150b113f8eb875915f38e67
Luiz <luizfvaccarif@gmail.com>
2021-05-08 00:31:51 +0100

    Requirement 4

--- script.js
+++ script.js
@@ -28,15 +28,35 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   return item.querySelector('span.item__sku').innerText;
 } 
 
+// R3 e R4
+const cartItems = '.cart__items'; // declaração no escopo global solicitada pelo Lint
+
+const cartLocalStorage = () => {
+  const cartList = document.querySelector(cartItems).innerHTML;
+  localStorage.setItem('cart', cartList);
+};
+
   function cartItemClickListener(event) {
   event.target.remove();
+  cartLocalStorage();
  }
+
+ const loadingLocalStorage = () => {
+  const savedCartItems = localStorage.getItem('cart');
+  const cartList = document.querySelector(cartItems);
+  cartList.innerHTML = savedCartItems;
+  cartList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('cart__item')) {
+      cartItemClickListener(event);
+    }
+  });
+};
  
   function createCartItemElement({ id: sku, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  // li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
@@ -62,8 +82,9 @@ const getData = () => {
 // R2
 
 const showCart = (productById) => {
-  const getCartItems = document.querySelector('.cart__items');
-  getCartItems.appendChild(createCartItemElement(productById));
+  const cartList = document.querySelector(cartItems);
+  cartList.appendChild(createCartItemElement(productById));
+  cartLocalStorage();
   };
 
 const getProductById = () => {
@@ -84,6 +105,7 @@ const getProductById = () => {
 };
 
 window.onload = function onload() { 
+  loadingLocalStorage();
   getData();
   getProductById();
 };
