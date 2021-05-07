71bbe2c003e1e5cb685d2302b15e0a8bddbe9843
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-07 13:04:25 -0300

    refatorando codigo

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// const fetch = require('node-fetch');
-
-async function listOfProducts() {
-  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
-  const data = await response.json();
-    return data.results;
-}
-
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -20,17 +12,17 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-// function createProductItemElement({ sku, name, image }) {
-  // const section = document.createElement('section');
-  // section.className = 'item';
+function createProductItemElement({ sku, name, image }) {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-  // section.appendChild(createCustomElement('span', 'item__sku', sku));
-  // section.appendChild(createCustomElement('span', 'item__title', name));
-  // section.appendChild(createProductImageElement(image));
-  // section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item__sku', sku));
+  section.appendChild(createCustomElement('span', 'item__title', name));
+  section.appendChild(createProductImageElement(image));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-  // return section;
-// }
+  return section;
+}
 
 // function getSkuFromProductItem(item) {
 //   return item.querySelector('span.item__sku').innerText;
@@ -49,7 +41,19 @@ function createCustomElement(element, className, innerText) {
 //   return li;
 // }
 
+async function listOfProducts() {
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const data = await response.json();
+    return data.results.map((product) =>
+    ({ sku: product.id, name: product.title, image: product.thumbnail }));
+}
+
+async function makeProduct() {
+  const computadores = await listOfProducts();
+  computadores.forEach((computador) =>
+  createProductItemElement(computador));
+}
+
 window.onload = async function onload() {
-  const produtos = await listOfProducts();
-  console.log(produtos);
+  makeProduct();
 };
