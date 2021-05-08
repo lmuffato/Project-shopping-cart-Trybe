cf68e338e99e355a891eec7a4e2a81204efab744
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-05-08 16:44:46 -0300

    corrige requisitos 2 e 3

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-window.onload = function onload() { };
+const api = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+const cart = document.querySelector('.cart__items');
 
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
@@ -8,9 +9,9 @@ function createProductImageElement(imageSource) {
 }
 
 function createCustomElement(element, className, innerText) {
-  const e = document.createElement(element);
-  e.className = className;
-  e.innerText = innerText;
+  const evento = document.createElement(element);
+  evento.className = className;
+  evento.innerText = innerText;
   return e;
 }
 
@@ -26,62 +27,57 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
- function getSkuFromProductItem(item) {
-   return item.querySelector('span.item__sku').innerText;
- }
+// function getSkuFromProductItem(item) {
+//   return item.querySelector('span.item__sku').innerText;
 
-  function cartItemClickListener(event) {
-    event.target.remove();
-  }
+//   li.addEventListener('click', cartItemClickListener);
+//   return li;
+// }
 
- function createCartItemElement({ sku, name, salePrice }) {
-   const li = document.createElement('li');
-   li.className = 'cart__item';
-   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-   li.addEventListener('click', cartItemClickListener);
-   return li;
- }
+function cartItemClickListener(event) {
+  const evento = event.target;
+  cart.removeChild(evento);
+}
 
-const informationsAPI = (interectAPI) => {
-  interectAPI.forEach((computer) => { 
-    const section = document.querySelector('.items');
-    section.appendChild(createProductItemElement(computer));
+function obtemProdutos() {
+  const produtos = fetch(api)
+  .then((response) => response.json())
+  .then((response) => response.results);
+
+  return produtos;
+}
+
+async function mostraProdutos() {
+  const produtos = await obtemProdutos();
+  const sectionItems = document.querySelector('.items');
+
+  produtos.forEach(({ id: sku, title: name, thumbnail_id: imageId }) => {
+      const image = `https://http2.mlstatic.com/D_NQ_NP_${imageId}-O.webp`;
+      sectionItems.appendChild(createProductItemElement({ sku, name, image }));
   });
+  itemAddEvent();
+}
+
+ //-----------------------------------------------------------------------------------------------------------------------------
+
+ async function selecionaItem(event) {
+  const evento = event.target;
+  const cart = document.querySelector('.cart__items');
+  const urlItem = `https://api.mercadolibre.com/items/${idItem}`;
+  const idItem = evento.parentElement.firstChild.textContent;
+  const { id: sku, title: name, price: salePrice } = await fetch(urlItem)
+  .then((response) => response.json())
+  .then((response) => response);
+
+  cart.appendChild(createCartItemElement({ sku, name, salePrice }));
+}
+
+function itemAddEvent() {
+  const buttons = document.querySelectorAll('.item__add');
+  buttons.forEach((button) => button.addEventListener('click', setItem));
+}
+
+window.onload = () => {
+  mostraProdutos();
+  selecionaItem();
 };
-
-const interectAPI = () => {
-  fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador').then((response) => {
-    response.json().then((data) => {
-      const informations = data.results;
-      console.log(informations);
-      informationsAPI(informations);
-    });
-  });
- };
-
-// -------------------------------------------------------------------------------
-const searchId = (id) => {
-  fetch(`https://api.mercadolibre.com/items/${id}`)
-  .then((response) => {
-    response.json()
-    .then((data) => {
-      const infoId = data;
-      createCartItemElement(infoId);
-      document.querySelector('.cart__items').appendChild(createCartItemElement(infoId));
-    });
-  });
-};
-
-const takeIdData = () => {
-  const route = document.querySelectorAll('.item__add');
-  route.forEach((button) => {
-    button.addEventListener('click', (event) => {
-      const pcId = getSkuFromProductItem(event.target.parentElement);
-      searchId(pcId);
-    });
-  });
-};
- window.onload = function onload() { 
-   interectAPI(); 
-   takeIdData();
- };
