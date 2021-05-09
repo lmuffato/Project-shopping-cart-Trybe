8b5db8b16802a3bed2e1c79747313ea9e4e4c3c4
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-09 17:24:15 -0300

    Adicionando requisito 2

--- script.js
+++ script.js
@@ -24,34 +24,49 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
-// function getSkuFromProductItem(item) {
-//   return item.querySelector('span.item__sku').innerText;
-// }
+function getSkuFromProductItem(item) {
+  return item.querySelector('span.item__sku').innerText;
+}
 
-// function cartItemClickListener(event) {
-//   // coloque seu código aqui
+ function cartItemClickListener(event) {
+  event.target.remove();
+}
 
-// }
-
-// function createCartItemElement({ sku, name, salePrice }) {
-//   const li = document.createElement('li');
-//   li.className = 'cart__item';
-//   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-//   li.addEventListener('click', cartItemClickListener);
-//   return li;
-// }
+function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+  const li = document.createElement('li');
+  li.className = 'cart__item';
+  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.addEventListener('click', cartItemClickListener);
+  return li;
+}
 
 async function listOfProducts() {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const data = await response.json();
-    return data.results.map((product) =>
+  return data.results.map((product) =>
     ({ sku: product.id, name: product.title, image: product.thumbnail }));
+}
+
+async function itemForItemCart(itemId) {
+  const response = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
+  const data = await response.json();
+  const { id, title, price } = data;
+  const result = { id, title, price };
+  return result;
+}
+
+async function addToCart(event) {
+  const idProduct = getSkuFromProductItem(event.target.parentNode);
+  const x = await itemForItemCart(idProduct);
+  const li = createCartItemElement(x);
+  document.querySelector('.cart__items').appendChild(li);
 }
 
 async function makeProduct() {
   const computadores = await listOfProducts();
   computadores.forEach((computador) => {
     const item = createProductItemElement(computador);
+    item.addEventListener('click', addToCart);
     document.querySelector('.items').appendChild(item);
   });
 }
