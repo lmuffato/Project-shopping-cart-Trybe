5a0cf50cf5ab5c7def2077e9c9dd43550b185f61
akirakiraa <andretaki2@gmail.com>
2021-05-10 00:53:33 -0300

    eslint round 2

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ function createProductImageElement(imageSource) {
   return img;
 }
 
+function cartItemClickListener(event) {
+  event.currentTarget.remove();
+}
+
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
-}
-
-function cartItemClickListener(event) {
-  event.currentTarget.remove();
 }
 
 async function fetchCartItem(itemId) {
@@ -50,9 +50,11 @@ function createProductItemElement({ sku, name, image }) {
   return section;
 }
 
+/*
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+*/
 
 console.log(fetchCartItem('MLB1341706310'));
 
