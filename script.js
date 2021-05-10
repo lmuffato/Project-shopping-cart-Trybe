47dc529b31d8d25c8343daf3b49ce55e6ab627e7
Junior Portela <portelajr14@gmail.com>
2021-05-10 01:39:17 -0300

    Req 4: recarregar local storage

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
+const getCart = () => document.querySelector('.cart__items');
+
 function boardKeys({ id, title, thumbnail }) {
-  const askedKeys = {
+  return {
     sku: id,
     name: title,
     image: thumbnail,
   };
-  return askedKeys;
 }
 
 function cartKeys({ id, title, price }) {
-  const askedKeys = {
+  return {
     sku: id,
     name: title,
     salePrice: price,
-  };
-
-  return askedKeys;
+  };  
 }
 
 function createProductImageElement(imageSource) {
@@ -47,10 +46,14 @@ const createProductItemElement = ({ sku, name, image }) => {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
+function toStorage() {
+  const content = getCart().innerHTML;
+  window.localStorage.setItem('cart', content);
+}
+
 function cartItemClickListener(event) {
-  const itemCart = event.target;
-  const getCart = document.querySelector('.cart__items');
-  getCart.removeChild(itemCart);
+  getCart().removeChild(event.target);
+  toStorage();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -66,11 +69,11 @@ const addToCart = async (event) => {
   const request = await fetch(`https://api.mercadolibre.com/items/${productId}`)
     .then((data) => data.json())
     .then((data) => {
-      const getCart = document.querySelector('.cart__items');
       const itemCart = createCartItemElement(cartKeys(data));
-      getCart.appendChild(itemCart);
+      getCart().appendChild(itemCart);
     });
-  
+
+  toStorage();
   return request;
 };
 
@@ -80,15 +83,30 @@ const getDataProducts = (product) => {
     .then((data) => data.results)
     .then((results) => {
       results.forEach((iten) => {
-        const info = boardKeys(iten);
+        const productInfo = boardKeys(iten);
         const getBoard = document.querySelector('.items');
-        const eachProduct = createProductItemElement(info);
+        const eachProduct = createProductItemElement(productInfo);
         eachProduct.querySelector('.item__add').addEventListener('click', addToCart);
         getBoard.appendChild(eachProduct);
-      });
+      }); // adicionar aqui a funçao de carregar o storage
     });
 };
 
+function loadStorage() {
+  const storedCart = localStorage.getItem('cart');
+  const updateCart = getCart();
+  updateCart.innerHTML = storedCart;
+  updateCart.addEventListener('click', (li) => {
+    if (li.target.classList.contains('cart__item')) {
+      cartItemClickListener(li);
+    }
+  });
+}
+// const setClick = document.querySelectorAll('cart__item');
+// setClick.forEach((li) => li.addEventListener('click', cartItemClickListener));
+
 window.onload = function onload() {
-  getDataProducts('computador'); 
+  getDataProducts('computador');
+  loadStorage();
+  // window.localStorage.removeItem('cart');
 };
