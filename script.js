231e1323c57fe1b83e7bbb103dd705d4fb4beccd
MZTBTG <mztbtg@hotmail.com>
2021-05-10 18:59:43 -0300

    Adicionando exercício 4

--- script.js
+++ script.js
@@ -28,18 +28,48 @@ function createProductItemElement({ sku, name, image }) {
 //   return item.querySelector('span.item__sku').innerText;
 // }
 
-function cartItemClickListener(event) {
+function cartClick(event) {
   const { target } = event;
+  const id = target.dataset.sku;
   target.remove();
+  const storageItens = JSON.parse(localStorage.getItem('cartItens'));
+  if (storageItens) {
+    const newArray = storageItens.filter((product) => product.sku !== id);
+    console.log(newArray);
+    localStorage.setItem('cartItens', JSON.stringify(newArray));
+  }
 }
+
+const validatorLocalStorage = () => {
+  const storageItens = localStorage.getItem('cartItens');
+  if (storageItens === null || storageItens === undefined) {
+    localStorage.setItem('cartItens', JSON.stringify([]));
+  } 
+};
 
 function createCartItemElement({ sku, name, salePrice }) {
   const li = document.createElement('li');
+  li.dataset.sku = sku;
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
-  li.addEventListener('click', cartItemClickListener);
+  li.addEventListener('click', cartClick);
   return li;
 }
+
+const chargeLocalStorage = () => {
+  const products = JSON.parse(localStorage.getItem('cartItens'));
+  products.forEach((product) => {
+    const li = createCartItemElement(product);
+    const ol = document.querySelector('.cart__items');
+     ol.appendChild(li);
+  });
+};
+
+const addItemToLocalStorage = ({ sku, name, salePrice }) => {
+  const storageItens = JSON.parse(localStorage.getItem('cartItens'));
+  storageItens.push({ sku, name, salePrice });
+  localStorage.setItem('cartItens', JSON.stringify(storageItens));
+};
 
 const fetchProductItem = async (e) => {
   const parent = e.target.closest('.item');
@@ -51,6 +81,7 @@ const fetchProductItem = async (e) => {
   const li = createCartItemElement({ sku, name, salePrice });
   const ol = document.querySelector('.cart__items');
   ol.appendChild(li);
+  addItemToLocalStorage({ sku, name, salePrice });
 };
 
 const handleCartAddClick = () => {
@@ -81,6 +112,8 @@ window.onload = function onload() {
   getObjectItems().then(() => {
     handleCartAddClick();
   });
+  validatorLocalStorage();
+  chargeLocalStorage();
 };
 
 // ....snumoc sarvalap òs oãs oãn snoitcnuF ooZ
